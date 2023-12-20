aa836dc3e8c1685b02e30ac569697425e10bace7
burak <burak.karaman.1994@gmail.com>
2023-12-20 11:17:27 +0100

    new js codes added

--- JavaScript/js-01-Intro-Variables/app.js
+++ JavaScript/js-01-Intro-Variables/app.js
@@ -1,1 +1,53 @@
-console.log("App js icindeyim")
+//console.log("App js içindeyim")  => tek satırlık yorum //
+
+// /*     */ Birden fazla satırı yoruma almak
+
+/*Birden 
+fazla 
+satır 
+yorumda */
+//! ÇIKTI ALMA VE KULLANICI İLE ETKİLEŞİM YÖNTEMLERİ
+
+// ************* Console Kullanımı ***********
+
+console.log("Konsol içindeyim")
+console.log("object")
+console.log("Hatanın altındaki satırdayım")
+console.error("Bu bir hata mesajıdır")
+console.warn("Bu bir uyarı mesajıdır")
+console.clear()
+
+console.table(
+    [
+        {ad:"Harvey",yas:35},
+        {ad:"Helen",yas:45}
+    ]
+)
+
+// ************ ALERT **************
+// alert("Bu bir uyarıdır")
+
+// yas=25 
+// if (yas<30) {
+//     alert("Biraz daha büyü de gel")
+// }
+
+// ************** DOCUMENT.WRITE() ********
+document.write("<b>Browserda sonuç gösterme </b> <br>")
+document.write("Bu ikinci satır <br>")
+
+a=15
+b=45
+toplam=a+b 
+document.write("Sonuc:",toplam,"<br>")
+console.log("sonuc:",toplam)
+
+// ********** PROPMT Kullanımı ***********
+// Kullanıcıdan bir değer almak için kullanılır
+//! web ortamında kullanıcıdan alınan bilgi her zaman metin(yazı) olarak algılanır
+
+
+ad="Burak"
+console.log(ad)
+
+
