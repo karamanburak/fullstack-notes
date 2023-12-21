9f2d900f446e25d21050ea63fd14a589bbe6eb0a
burak <burak.karaman.1994@gmail.com>
2023-12-21 12:35:26 +0100

    js data types added

--- JavaScript/js-02-Data-Types/data.types.js
+++ JavaScript/js-02-Data-Types/data.types.js
@@ -131,5 +131,136 @@ console.log(Math.sqrt(56).toFixed(2))
 let küpkok=125**(1/3)
 console.log("küpkök",küpkok)
 console.log(Math.cbrt(125))
+console.clear()
 
 /* -------------------------------------------------------------------------- */
+
+/* ---------------------------------- */
+//!          STRING DATA TYPE         //
+/* ---------------------------------- */
+// Tirnaklar icinde ifade edilen herhangi bir deger stringdir.
+// stringler harf,isaretler,sayilar
+let ad="Javascript"
+console.log(ad)
+
+console.log(ad [0])
+console.log(ad [3])
+console.log(ad [7])
+
+//immutable
+ad[7]="u"
+console.log(ad)
+
+ad="Node.js"
+console.log(ad)
+
+/* -------------------------------------------------------------------------- */
+// Escape characters // Kacis karakterleri
+// \ kacis karakterleri \ ile tanimlanir
+// \n yeni bir satira gecmeyi saglar
+// \t tab islemi yapar (8 karakter bosluk birakir)
+
+let deger="Merhaba, bu Elmas\'in ödevidir."
+console.log(deger)
+let siir="Yasamak tek bir agac gibi hür ve\nbir orman gibi kardescesine"
+console.log(siir)
+
+let developer="Frontend\tDeveloper"
+console.log(developer)
+console.clear()
+
+
+/* -------------------------------------------------------------------------- */
+
+//! String birlestirme
+
+// 1.concat methodu
+
+let adi="Ali"
+let soyad="Güclü"
+
+// string1.concat(string2)
+console.log(adi.concat(soyad))
+/* -------------------------------------------------------------------------- */
+// 2. + isareti ile concatanation (stringleri birlestirme islemi)
+
+console.log(adi+soyad)
+console.log("Adiniz Soyadiniz:"+adi+soyad)
+
+//! Template literal yöntemi :=> ES6 ile gelen özelliklerden
+
+// stringleri dinamik olarak kullanmayi saglar.
+// stringler yazilirken degiskenlerde kullanabilmemizi saglar
+
+adi="Ali"
+soyad="Seven"
+let job="Fullstack developer"
+
+// 2. yöntem ile
+console.log("Personelimiz "+adi+" "+soyad+" kurumumuzda "+job+" olarak calismaktadir.");
+// template literal yöntemi ile
+console.log(`Personelimiz ${adi} ${soyad} kurumumuzda ${job} oalrak calismatadir`);
+
+
+/* -------------------------------------------------------------------------- */
+console.clear()
+ /* ---------------------------------- */
+ /*          BOOLEAN DATA TYPE         */
+ /* ---------------------------------- */
+// true - false degeri tutar
+
+let isOpen=true
+console.log(typeof isOpen)
+
+let isSmall=4>15
+console.log(isSmall)
+
+sifre="3de4"
+
+let kontrol=(sifre==="3de4")
+console.log(kontrol)
+console.log(typeof kontrol)
+
+
+/* -------------------------------------------------------------------------- */
+ /* ---------------------------------- */
+ /*         UNDEFINED DATA TPYE        */
+ /* ---------------------------------- */
+
+ //bir degiskene bir deger atamadigimiz zaman undefined
+ // bir fonksiyon geri deger döndürmüyorsa undefined olur
+
+ // undefined: tanimsiz
+
+ let val
+ console.log(val)
+
+ val=345
+ console.log(val)
+
+
+/* -------------------------------------------------------------------------- */
+/* ---------------------------------- */
+/*           NULL DATA TYPE           */
+/* ---------------------------------- */
+
+let empty=null
+console.log(empty)
+
+// js bug
+console.log(typeof empty)
+
+
+/* -------------------------------------------------------------------------- */
+/* ---------------------------------- */
+/*       NONPRIMITIVE DATA TYPE       */
+/* ---------------------------------- */
+// referans tipli degiskenler
+//array
+let liste=["kalem","silgi", "defter"]
+console.log(liste);
+
+// Object
+let nesne={ad:"Ali",soyad:"Güven",yas:"29"}
+console.log(nesne);
+
