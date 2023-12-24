754b1e09f6577a585088246118e8ff748194fa6c
burak <burak.karaman.1994@gmail.com>
2023-12-24 17:00:04 +0100

    examples data.js added

---
+++ JavaScript/js-02-Data-Types/example.js
@@ -0,0 +1,15 @@
+
+const str1 = "Merhaba"
+const str2 = "Dünya"
+
+// console.log(str1, str2);
+const birlesikString = str1.concat("", str2)
+console.log(birlesikString);
+
+console.clear()
+
+let str3 = "Hello"
+let str4 = "World"
+
+let birlesikString2 = str3 + ' ' + str4
+console.log(birlesikString2);
