7fb30e568940499c57c95080cb009d8fe57cb603
burak <burak.karaman.1994@gmail.com>
2023-12-31 15:57:16 +0100

    switch case method added

--- Team Works/09-JS-simple-Calculator/index.js
+++ Team Works/09-JS-simple-Calculator/index.js
@@ -2,22 +2,53 @@
     /*        SIMPLE JS CALCULATOR        */
     /* ---------------------------------- */
 
+// const number1 = parseFloat(prompt('Enter your first number!'))
+// const operator = prompt('Enter an operator (+, -, *, /)')
+// const number2 = parseFloat(prompt('Enter your second number!'))
+
+
+// if (operator === '+' ) {
+//     result = number1 + number2
+// } else if (operator === '-') {
+//     result = number1 - number2
+// } else if (operator === '*') {
+//     result = number1 * number2
+// } else {
+//     result = number1 / number2
+// }
+
+// console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+
+/* -------------------------------------------------------------------------- */
+/*                                Switch Method                               */
+/* -------------------------------------------------------------------------- */
+
+
 const number1 = parseFloat(prompt('Enter your first number!'))
-const operator = prompt('Enter an operator (+, -, *, /)')
+const operator =  prompt('Enter an operator between (+ | - | * | / |) ')
 const number2 = parseFloat(prompt('Enter your second number!'))
 
+switch (operator){
+    case '+':
+        result = number1 + number2
+        break
+    case '-':
+        result = number1 - number2
+        break
+    case '*' :
+        result = number1 * number2
+        break
+    case '/':
+        result = number1 / number2
+        break
+    default:
+        result = 'Invalid result'
 
-if (operator === '+' ) {
-    result = number1 + number2
-} else if (operator === '-') {
-    result = number1 - number2
-} else if (operator === '*') {
-    result = number1 * number2
-} else {
-    result = number1 / number2
 }
 
-console.log(`${number1} ${operator} ${number2} = ${result}`);
+console.log(`Result is : ${number1} ${operator} ${number2} = ${result}`);
+    
 
 
 //  Q3.  Interpret the flow diagram below for the case where income = 2000 and cost = 1200.
@@ -44,4 +75,4 @@ console.log(`${number1} ${operator} ${number2} = ${result}`);
 //     result = NaN
 // }
 
-// console.log(`${cost} - ${income}  = ${result}`);
+// console.log(`${cost} - ${income}  = ${result}`)
