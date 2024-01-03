56657f9626b83441df16075de5a9d74e4e2eb1e7
burak <burak.karaman.1994@gmail.com>
2024-01-03 15:27:47 +0100

    new js examples added

---
+++ JavaScript/assignments/03/script.js
@@ -0,0 +1,91 @@
+/* ---------------------------------- */
+/*             1. EXAMPLE             */
+/* ---------------------------------- */
+
+// const number = prompt("Please enter a number between (1-12)")
+// let result
+
+// switch (number) {
+//     case '1':
+//         result = "january"
+//         break
+//     case '2':
+//         result = "february"
+//         break
+//     case '3':
+//         result = "march"
+//         break
+//     case '4':
+//         result = "april"
+//     case '5':
+//         result = "may"
+//         break
+//     case '6':
+//         result = "juni 😎"
+//         break
+//     case '7':
+//         result = "juli"
+//         break
+//     case '8':
+//         result = "august"
+//         break
+//     case '9':
+//         result = "september"
+//         break
+//     case '10':
+//         result = "october"
+//         break
+//     case '11':
+//         result = "november"
+//         break
+//     case '12':
+//         result = "december"
+//         break
+//     default:
+//         result = "Please enter a valid Number!"
+// }
+
+
+// console.log(`Your lucky month is : ${result}`);
+
+
+/* ---------------------------------- */
+/*             2: EXAMPLE             */
+/* ---------------------------------- */
+
+
+// const number1 = parseInt(+prompt("Enter your first number!"))
+// const number2 = parseInt(+prompt("Enter your second number!"))
+// const number3 = parseInt(+prompt("Enter your third number!"))
+// const plus = number1 + number2 + number3
+// const multiply = number1 * number2 * number3
+// let result
+// let result2
+
+
+
+
+// if (number1 <= number2 && number1 <= number3) {
+//     result = "Number1 is smallest"
+// } else if (number2 <= number1 && number2 <= number3) {
+//     result = "Number2 is smallest"
+// } else if (number3 <= number1 && number3 <= number2) {
+//     result = "Number3 is smallest"
+// }
+
+// if (number1 >= number2 && number1 >= number3) {
+//     result2 = "Number1 is biggest"
+// } else if (number2 >= number1 && number2 >= number3) {
+//     result2 = "Number2 is biggest"
+// } else if (number3 >= number1 && number3 >= number2) {
+//     result2 = "Number3 is biggest"
+// } else {
+//     result = "Try Again!"
+// }
+
+// console.log(`Addition : ${plus}`);
+// console.log(`Multiplication : ${multiply}`);
+// console.log(`Smallest : ${result}`);
+// console.log(`Biggest : ${result2}`);
+
+
