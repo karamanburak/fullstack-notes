9434c1a575142aa944fdb45fa15bd7da292b7578
burak <burak.karaman.1994@gmail.com>
2024-01-03 12:06:02 +0100

    new examples added

--- JavaScript/js-04-Conditions/script.js
+++ JavaScript/js-04-Conditions/script.js
@@ -52,7 +52,7 @@
 
 // sonuc && console.log(result);
 
-//! TERNARY 
+//! TERNARY
 
 // const age = 22;
 // const gender = "erkek"
@@ -64,10 +64,10 @@
 //     console.log("Askerlik yapamaz 😒");
 // }
 
-// //! 2. yol ternary 
+// //! 2. yol ternary
 // age >= 20 && gender == "erkek" && health == true ? console.log("askerlik yapablilir 😎") : console.log("askerlik yapamaz 😒");
 
-// //? örnek ternary 
+// //? örnek ternary
 
 // const grade1 = prompt("Please enter your grade")
 
@@ -82,16 +82,94 @@
 // //? Nested ternary yazmak aslinda anlasilabilirligi azaltiyor. Bu sebeple, sadece 2 durum var ise
 // //? Ternary kullanmak daha mantiklidir, yoksa if tercih edilebilir.
 
-const speed = 10
+// const speed = 10
 
-speed > 120 ? console.log("Speedy") : speed >= 90 ? console.log("Normal") : console.log("Low Speed");
+// speed > 120 ? console.log("Speedy") : speed >= 90 ? console.log("Normal") : console.log("Low Speed");
 
 
-// //? ayni örneğin if-else ile yapilmasi
-if (speed > 120) {
-    console.log("Speedy")
-} else if (speed >= 90) {
-    console.log("Normal")
-} else {
-    console.log("Low Speed")
+// // //? ayni örneğin if-else ile yapilmasi
+// if (speed > 120) {
+//     console.log("Speedy")
+// } else if (speed >= 90) {
+//     console.log("Normal")
+// } else {
+//     console.log("Low Speed")
+// }
+
+//**************** */ TERNARY
+
+//? ornek: maasınız asgari ücretten düşük ve eşitse maaşınıza %25 zam yapılsın, değilse maaşınıza %10 zam yapılsın.
+
+// const asgariUcret = 17000
+// const maas = +prompt("Lütfen maasinizi giriniz!")
+
+// const zamliMaas = maas <= asgariUcret ? maas * 1.25 : maas * 1.10
+// console.log(zamliMaas);
+
+// const maas = 10000
+// const borc = 95000
+
+// const krediDurumu = maas * 10 - borc >= 5000 ? console.log("Kredi alabilirsiniz!") : console.log("Borcunuzdan dolayi kredi cekemezsiniz");
+
+// const maas = +prompt("maasinizi giriniz")
+// const borc = +prompt("borcunuzu giriniz")
+
+// const krediDurumu = maas * 10 -borc >= 5000 ? console.log("Kredi alabilirsiniz") : console.log("kredi cekemezsiniz");
+
+
+
+//? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
+//? yazdiriniz.
+
+
+// const number1 = parseInt(prompt("Enter your first number!"))
+// const number2 = parseInt(prompt("Enter your second number!"))
+// const number3 = parseInt(prompt("Enter your third number!"))
+
+
+// const n1 = +prompt("number1")
+// const n2 = +prompt("number2")
+// const n3 = +prompt("number3")
+
+//? kosullara logical operatorler yardimiyla birlestirilmesi
+
+// if (n1 > n2 && n1 > n3) {
+//     console.log("biggest:", n1);
+// } else if (n2 > n1 && n2 > n3) {
+//     console.log("biggest : ", n2);
+// } else if (n3 > n1 && n3 > n1) {
+//     console.log("biggest :", n3);
+// } else console.log("PLease Try Again!");
+
+//! 2. yöntem
+
+// let biggest = n1
+
+// if (n2 > biggest) {
+//     biggest = n2
+// }
+// if (n3 > biggest) {
+//     biggest = n3
+// }
+
+// console.log(biggest);
+
+
+const not = +prompt("Notu girin:");
+
+if (not < 0 || not > 100) {
+    console.log(" 0-100 aralığında bir sayı girin.");
+} else if (not >= 0 && not <= 25) {
+    console.log("Not: FF");
+} else if (not >= 26 && not <= 45) {
+    console.log("Not: DD");
+} else if (not >= 46 && not <= 65) {
+    console.log("Not: CC");
+} else if (not >= 66 && not <= 75) {
+    console.log("Not: BB");
+} else if (not >= 76 && not <= 90) {
+    console.log("Not: BA");
+} else if (not >= 91 && not <= 100) {
+    console.log("Not: AA");
 }
+
