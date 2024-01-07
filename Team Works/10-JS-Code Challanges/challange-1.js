7c3ecdbb3ca6e0ef590be3ce315f9ab65f7da3f8
burak <burak.karaman.1994@gmail.com>
2024-01-07 13:07:25 +0100

    Challange 2 rules added

--- Team Works/10-JS-Code Challanges/challange-1.js
+++ Team Works/10-JS-Code Challanges/challange-1.js
@@ -1,3 +1,8 @@
+/* ---------------------------------- */
+/*            CHALLANGE - 1           */
+/* ---------------------------------- */
+
+
 // Calculate the Greatest Common Divisor (GCD)
 // Objective : Write a JavaScript program that calculates and prints the Greatest Common Divisor (GCD) of two
 // given numbers using only conditional statements (if, else if, else) and loops (for, while).
@@ -9,20 +14,49 @@
 //     - The program should prompt the user for two numbers, a and b.
 //     - Display the calculated GCD on the console.
 
-let a = +prompt("Please enter your first number!");
-let b = +prompt("Please enter your second number!");
-let gcd
+// let a = +prompt("Please enter your first number!");
+// let b = +prompt("Please enter your second number!");
+// let gcd
 
-while (a != b) {
-    if (a > b) {
-        a = a - b
-    } else {
-        b = b - a
-    }
-}
+// while (a != b) {
+//     if (a > b) {
+//         a = a - b
+//     } else {
+//         b = b - a
+//     }
+// }
 
-gcd = a
+// gcd = a
 
-console.log(`GCD is : ${gcd} `);
+// console.log(`GCD is : ${gcd} `);
+
+
+/* ---------------------------------- */
+/*            CHALLANGE - 2           */
+/* ---------------------------------- */
+
+// Armstrong Number Checker
+// Objective : Write a JavaScript program to check if a given number is an Armstrong number using only
+// conditional statements (if, else if, else) and loops (for, while).
+
+// Rules :
+
+// - An Armstrong number (also known as narcissistic number) of three digits is an
+// integer such that the sum of the cubes of its digits is equal to the number itself.
+// - You can only use conditional statements (if, else if, else) and loops (for,
+// while, do while).
+// - The program should prompt the user for a number.
+// - Display whether the entered number is an Armstrong number or not.
+
+// Example Armstrong numbers:
+// . 153: 13+53+33 = 1 + 125 + 27 = 153
+// . 370: 33+73+03 = 27 + 343 + 0 = 370
+// . 371: 33+73+13 = 27 + 343 + 1 = 371
+// . 407: 43+03+73 = 64 + 0 + 343 = 407
+// . 1634: 14+64+34+44 = 1 + 1296 + 81 +256 = 1634
+// . 8208
+// . 9474
+// . 54748
+
 
 
