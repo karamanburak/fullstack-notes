eeab6889e9d5a45d99ab483ed952cd5e4636872a
burak <burak.karaman.1994@gmail.com>
2024-01-09 10:25:56 +0100

    updated

--- JavaScript/js-05-Loops/loops.js
+++ JavaScript/js-05-Loops/loops.js
@@ -28,13 +28,42 @@
 
 
 
-const sayi = +prompt("Lütfen asal sayi kontrolü icin bir sayi giriniz!")
-let flag = true
+// const sayi = +prompt("Lütfen asal sayi kontrolü icin bir sayi giriniz!")
+// let flag = true
 
-for (let i = 2; i < sayi; i++) {
-    if (sayi % i == 0) {
-        flag = false
-    }
+// for (let i = 2; i < sayi; i++) {
+//     if (sayi % i == 0) {
+//         flag = false
+//     }
+// }
+// console.log(flag ? "Girdiginiz sayi asaldir👌" : "Girdiginiz sayi asal degildir!");
+
+// //? Ornek: 0-100 Arasinda 7 adet rastgele tamsayi sayi ureten kodu
+// //? for dongusu ile yaziniz..
+
+// const rast = Math.random() * 100;
+// const rast2 = Math.ceil(rast)
+// console.log(rast, rast2);
+
+
+for (let i = 1; i <= 7; i++) {
+    const random = Math.round(Math.random() * 100)
+    console.log(random);
 }
-console.log(flag ? "Girdiginiz sayi asaldir👌" : "Girdiginiz sayi asal degildir!");
+
+
+
+//* NOT:
+//* Math.floor() =>en yakin alt tamsayiya yuvarlar
+//* Math.ceil() =>en yaki ust tamsayiya yuvarlar
+//* Math.round() => degerine gore nereye yakınsa oraya yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
+
+
+
+
+
+
+
 
