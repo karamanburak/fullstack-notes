7d6211c0e0d06133d463cbddf071647ec5d6ccfd
burak <burak.karaman.1994@gmail.com>
2024-01-10 14:01:28 +0100

    fibonacci number example added

--- Team Works/10-JS-Code Challanges/challange-1.js
+++ Team Works/10-JS-Code Challanges/challange-1.js
@@ -1,9 +1,10 @@
 /* ---------------------------------- */
-/*            CHALLANGE - 1           */
+//^            CHALLANGE - 1          */
 /* ---------------------------------- */
 
 
-// Calculate the Greatest Common Divisor (GCD)
+//!  Calculate the Greatest Common Divisor (GCD)
+
 // Objective : Write a JavaScript program that calculates and prints the Greatest Common Divisor (GCD) of two
 // given numbers using only conditional statements (if, else if, else) and loops (for, while).
 
@@ -31,11 +32,69 @@
 // console.log(`GCD is : ${gcd} `);
 
 
+//^ WHILE VERSION
+
+// const num1 = +prompt("Lütfen 1. sayiyi giriniz")
+// const num2 = +prompt("Lütfen 2. sayiyi giriniz")
+// let gcd
+
+// for (let i = num1 < num2 ? num1 : num2; i <= num1 && i <= num2; i--) {
+//     if (num1 % i === 0 && num2 % i === 0) {
+//         gcd = i;
+//         break
+//     }
+// }
+
+// console.log(`The GCD of the entered numbers is : ${gcd}`);
+
+
+
+
+
 /* ---------------------------------- */
-/*            CHALLANGE - 2           */
+//^          CHALLANGE  - 2           */
 /* ---------------------------------- */
 
-// Armstrong Number Checker
+//! FIBONACCI NUMBER
+
+// Write a JavaScript program that computes and prints the first n Fibonacci numbers without
+// using any built-in functions (like Math.pow, Math.sqrt, Array methods, etc.) or arrays.
+// Rules:
+
+// - You must not use any built-in JavaScript functions related to Fibonacci sequences
+// or arrays.
+// - The program should prompt the user for the value of n.
+// - Display the first n Fibonacci numbers.
+
+// Example: For n = 7, the Fibonacci sequence would be: 0, 1, 1, 2, 3, 5, 8
+
+// const num = parseInt(prompt("Lütfen bir sayi giriniz!"))
+
+// if (num === 1) {
+//     console.log('0');
+// } else if (num === 2) {
+//     console.log('0,1');
+// } else {
+//     let a = 0;
+//     let b = 1;
+//     let output = '0,1';
+//     for (let i = 3; i <= num; i++) {
+//         let c = a + b;
+//         output += ',' + c;
+//         a = b;
+//         b = c;
+//     }
+//     console.log(`Ilk ${num} fibonacci serisi : ` + output);
+// }
+
+
+
+/* ---------------------------------- */
+//^            CHALLANGE - 3          */
+/* ---------------------------------- */
+
+//! Armstrong Number Checker
+
 // Objective : Write a JavaScript program to check if a given number is an Armstrong number using only
 // conditional statements (if, else if, else) and loops (for, while).
 
@@ -57,6 +116,5 @@
 // . 8208
 // . 9474
 // . 54748
-
 
 
