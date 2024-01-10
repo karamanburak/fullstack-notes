86057b07c4c6372230d83102a8c58005667ed388
burak <burak.karaman.1994@gmail.com>
2024-01-10 14:43:47 +0100

    new example add

--- Team Works/10-JS-Code Challanges/challange-1.js
+++ Team Works/10-JS-Code Challanges/challange-1.js
@@ -118,15 +118,37 @@
 // . 54748
 
 
-const num = prompt("Lütfen bir sayi giriniz!")
-const n = num.length
-let sum = 0;
-for (let i = 0; i < n; i++) {
-    sum += num[i] ** n
-}
-if (sum === +num) {
-    console.log(`Girilen sayi bir Armstrong sayisidir!`);
-} else {
-    console.log(`Girilen sayi bir Armstrong sayisi degildir!`);
+// const num = prompt("Lütfen bir sayi giriniz!")
+// const n = num.length
+// let sum = 0;
+// for (let i = 0; i < n; i++) {
+//     sum += num[i] ** n
+// }
+// if (sum === +num) {
+//     console.log(`Girilen sayi bir Armstrong sayisidir!`);
+// } else {
+//     console.log(`Girilen sayi bir Armstrong sayisi degildir!`);
 
+// }
+
+let num = parseInt(prompt("Enter a number:"));
+let originalNumber = num;
+let digitCount = 0;
+let sum = 0;
+
+while (originalNumber !== 0) {
+    originalNumber = Math.floor(originalNumber / 10)
+    console.log(originalNumber)
+    digitCount++
 }
+
+originalNumber = num;
+
+while (originalNumber > 0) {
+    let digit = originalNumber % 10;
+    sum += digit ** digitCount;
+    originalNumber = Math.floor(originalNumber / 10)
+}
+
+sum === num ? console.log(num + " is an Armstrong number.") : console.log(num + " is not an Armstrong number."
+)
