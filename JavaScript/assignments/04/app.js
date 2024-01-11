6816fbad8cffaec143464bda0d1c2e5c7d607657
burak <burak.karaman.1994@gmail.com>
2024-01-11 16:07:41 +0100

    new assignment added

---
+++ JavaScript/assignments/04/app.js
@@ -0,0 +1,46 @@
+//^ EXAMPLE - 1 
+//? Taban ve yükseklik degerlerini parametre olarak alan ve bir ücgenin alanini hesaplayarak ana programa döndüren fonksiyonu yaziniz.
+
+const ucgenAlan = function (yükseklik, taban) {
+
+    let ucgenAlan = (1 / 2) * (yükseklik * taban)
+
+
+    return ucgenAlan
+
+}
+
+console.log(ucgenAlan(10, 20));
+
+
+
+//^ EXAMPLE - 2
+//? kareAl, küpAl, üsAl seklinde üc adet farkli arrow fonkisiyonu tanimlayin. Bu fonksiyonlarin ana programdan gereken parametreleri alarak sonuclari ana programa döndürmeleri gerekmektedir.
+
+let sayi = +prompt("Lütfen islem yapmak istediginiz sayiyi giriniz!")
+
+const kareAl = () => sayi ** 2
+const küpAl = () => sayi ** 3
+const üsAl = () => sayi ** sayi
+console.log(kareAl())
+console.log(küpAl())
+console.log(üsAl())
+
+//^ EXAMPLE - 3
+// //? Yil degerini parametre olarak alan ve bu yilin artik yil olup olmadigini hesaplayarak sonucu ana programa döndüren fonksiyonu function-expression yöntemi ile yaziniz. NOT: Yil 4'e tam bölünüyorsa VE (100'e tam bölnmüyorsa VEYA 400'e tam bölünüyorsa) artik yildir aksi takdirde degildir.
+
+const artikYil = function (yil) {
+    yil = +prompt("Lütfen bir yil giriniz!");
+
+    if (yil % 4 == 0 && yil % 100 != 0 || yil % 400 == 0)
+        yil = `${yil} bir artik yildir`
+    else
+        yil = `${yil} bir artik yil degildir.`
+
+    return yil
+}
+
+console.log(artikYil());
+
+
+
