f5d2ffd3a5671321e06e165f4518e219b2e7f896
burak <burak.karaman.1994@gmail.com>
2024-01-12 11:50:16 +0100

    new example added

--- JavaScript/js-07-Functions/5-scope.js
+++ JavaScript/js-07-Functions/5-scope.js
@@ -28,3 +28,62 @@ const scopeFuntion = () => {
 console.log(scopeFuntion());
 console.log(first);
 console.log(second);
+
+//^ EXAMPLE - 2 
+//! global degisken 
+
+let number1 = 11;
+
+const func1 = () => {
+
+    let number1 = 44 //! function-scope
+
+    number1++
+
+    return number1
+
+}
+
+console.log(func1());
+
+//? fonksiyondan gelen değer number1 değişkenine atandı
+number1 = func1() //?global scope ama 45 olarak değişti
+
+// const number1 = 56 //! Uncaught SyntaxError: Identifier 'number1' has already been declared
+
+
+{
+    number1++
+
+    let x = 5;
+
+    x++
+
+    let y = x + 15
+
+    console.log(y); //? blocked-scope 
+
+
+}
+
+console.log(number1);
+
+
+
+//? VAR 
+
+number2 = 100;
+
+var number2; //? hoisted (en yukariya tasimis oluruz.)
+
+var result = "Hallo"
+
+if (number2 == 100) {
+
+    let result = "Hi!" //? local(blocked)
+    console.log(result);
+} else {
+    result = "Salute" //? global
+    console.log(result);
+}
+console.log(result); //? global
