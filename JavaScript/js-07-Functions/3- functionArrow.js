be75d4e27503dac978080f1ffb0ab86efe19da6c
burak <burak.karaman.1994@gmail.com>
2024-01-12 09:44:24 +0100

    update

--- JavaScript/js-07-Functions/3- functionArrow.js
+++ JavaScript/js-07-Functions/3- functionArrow.js
@@ -15,3 +15,49 @@ console.log("****** 3- ARROW FUNCTIONS ******");
 const control = (sayi) => sayi % 3 == 0 ? "3'ün katidir" : "3'ün kati degildir"
 
 console.log(control(25));
+
+//* Örnek2:Menu fonksiyon
+// ! ok(arrow) fonksiyonunda birden fazla ifade varsa, fonksiyonda süslü parantez kullanmalıyız
+//****************************
+
+const menu = () => {
+    console.log("===========================");
+    console.log("     JAVASCRİPT DERSİ      ");
+    console.log("===========================");
+};
+
+menu()
+
+//* EXAMPLE - 3
+//? silindirin hacmini hesaplayin! (pi*r**2*h)
+
+const hacimHesapla = (r, h) => Math.PI * r * r * h
+
+console.log(hacimHesapla(3, 7))
+console.log(hacimHesapla(4, 9).toFixed(4))
+// toFixed(a)=virgüllü bir sayıda, virgülden sonra istediğimiz adette (a) rakam görmemizi sağlar
+
+//* ORNEK5: Girilen n. terimdeki  Fibonacci sayisını  yazdiran fonksiyonu dongu ile kodlayiniz ve bu n sayisina kadar olan elemanlari toplayiniz!
+//? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+
+const fibonacciNumber = (n) => {
+
+    let fiboSol = 1
+    let fiboSag = 1
+    let yeniFibo = 0
+    let toplam = 2
+
+    for (let i = 1; i < n - 1; i++) {
+
+        yeniFibo = fiboSol + fiboSag
+
+        fiboSol = fiboSag
+        fiboSag = yeniFibo
+        toplam += yeniFibo
+    }
+    console.log(toplam);
+    return yeniFibo
+}
+
+console.log(fibonacciNumber(6));
