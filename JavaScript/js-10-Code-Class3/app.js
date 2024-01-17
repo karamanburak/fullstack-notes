46507f10a0deb63a21bed374fae21c6fd5de4bbe
burak <burak.karaman.1994@gmail.com>
2024-01-17 09:39:40 +0100

    add code class 3

---
+++ JavaScript/js-10-Code-Class3/app.js
@@ -0,0 +1,46 @@
+// let str = 'Hello World'
+// let uzunluk = (str) => {
+//     return str.length
+// }
+// for (let i = 0; i <= uzunluk(str) - 1; i++) {
+//     console.log(str[i]);
+// }
+
+
+// let lorem = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odio dolorum sunt quaerat illum tempora alias magnam excepturi numquam atque eaque officia soluta ullam eius nesciunt, sed quos, in culpa optio autem voluptates itaque eveniet quidem!`
+
+// console.log(lorem.length);
+// console.log(lorem[201]);
+// console.log(lorem.charAt(201));
+
+// if (lorem.charAt(201) === 'o') {
+//     console.log(`yes`);
+// } else {
+//     console.log(`no`);
+// }
+
+
+// let lorem2 = `Lorem ipsum dolor sit amet consectetur adipisicing elit.`
+
+// console.log(lorem2.charCodeAt(0));
+// console.log(lorem2.charCodeAt(19));
+
+// Sezar sifrelemesi. String alacak ve ikinci parametre sayi olacak. O sayi kadar kaydirma yapacak.
+
+function cipher(str, shift) {
+    let result = ''
+
+
+    for (let i = 0; i < str.length; i++) {
+
+        let ciphedText = str.charCodeAt(i) + shift
+        result += String.fromCharCode(ciphedText)
+    }
+    console.log(result);
+
+}
+
+console.log(cipher('abc', 1));
+
+
+
