e0dbfe1cea2f3d4df55afddbb300748ffec5bf19
burak <burak.karaman.1994@gmail.com>
2024-01-22 11:22:01 +0100

    addd new code challenges

--- JavaScript/js-13-Code-Class4/app.js
+++ JavaScript/js-13-Code-Class4/app.js
@@ -8,7 +8,7 @@ reverse(): Diziyi tersine cevirir.
 splice(): (Baslangic indeksi, kac eleman silinecek, eklenecek eleman(item1,item2...))
 sort(): siralama yapar. (stringlerdeki ifadelerde ascii kodlarina göre alfabetik siralama)
 sort ((a-b)=> a-b)
-include(deger): iceriyor mu? (true - false)
+includes(deger): iceriyor mu? (true - false)
 indexOf(deger): degerin kacinci indexte oldugunu verir.
 join(" "): bir diziyi birlestirerek stringe dönüstürür.
 slice(): diziden bir parcayi almamizi saglar. 
@@ -89,4 +89,46 @@ function countWords2(sentence) {
     return result
 }
 
-console.log(countWords2(sentence));
+console.log(countWords2(sentence2));
+
+
+//!Size bir  kelime verildi. "Clarusway" kelimesini bulmanız ve şuna benzer bir cümle döndürmeniz gerekir: 
+
+//? "[Clarusway'i bulduğunuz kelimenin sırası] 'nda Clarusway'i buldum!"
+
+//? Clarusway'i bulamıyorsanız, "I can't find Clarusway :(" yazın.
+
+//? findClarusway("I am finding Clarusway !") ➞ "I found Clarusway at 4!"
+
+const newSentence = "I like Clarusway"
+
+const findWord = (sentence) => {
+    if (sentence.includes("Clarusway")) {
+        return `I found Clarusway at ${sentence.split(" ").indexOf("Clarusway") + 1} `;
+    } else {
+        return "I can't find Clarusway :(";
+
+    }
+
+}
+console.log(findWord(newSentence));
+
+//! Bir öğe dizisini alan, tüm yinelenen öğeleri kaldıran ve eski diziyle aynı sırada yeni bir dizi döndüren bir fonksiyon oluşturan program 
+
+const removeDuplicated = ([1, 0, 1, 0, 1]) // Output [1,0]
+const removeDuplicated2 = (["the", "small", "cat", "the", "cat"]) // ouput["the","small","cat"]
+
+
+
+function removeDuplicates(arr) {
+    let unique = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (unique.indexOf(arr[i]) === -1) {
+            unique.push(arr[i]);
+        }
+    }
+    return unique;
+}
+
+console.log(removeDuplicates(removeDuplicated));
+console.log(removeDuplicates(removeDuplicated2));
