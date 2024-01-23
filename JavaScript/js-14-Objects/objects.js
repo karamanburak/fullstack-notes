ab0cc484060637f0a5cdfa5bc177b7c5bc47f36e
burak <burak.karaman.1994@gmail.com>
2024-01-23 10:00:30 +0100

    add new "key-values" in objects

--- JavaScript/js-14-Objects/objects.js
+++ JavaScript/js-14-Objects/objects.js
@@ -43,3 +43,55 @@ const personel2 = new personelList(1002, "Elmas", 20000)
 
 console.log(personel1, personel2);
 //! Her nesne icin ayri bir kopya olusturulan metodlar, bellek kullanimini arttirir. 
+
+
+//? 3. OBJECT LITERAL ( en cok kullanilan yöntem)
+
+let personelYapisi = {} // bos bir object üretmek icin kullanilan metod
+console.log(personelYapisi);
+
+let personel = {
+
+    name: "Burak",
+    lastName: "Karaman",
+    birthDate: 1994,
+    isMarried: true,
+    experiences: ["tester", "developer", "devops"],
+    address: {
+        city: "potsdam",
+        street: "AbzweigBetriebshof",
+        no: 50
+    },
+
+
+    calculateAge: function () {
+        return new Date().getFullYear() - this.birthDate
+    },
+
+    hi: () => {
+        return "Hello"
+    },
+
+    arrowFunction: () => {
+        return this.isMarried
+    }
+
+    // Arrow function üretilirken this keyword ünden kurtulmak için yapılmıştır.
+    // Arrow functionda this window objesine karşılık gelir.
+    // windows objesi içinde workExperiences isimli bir özellik bulamadığından undefined
+}
+
+
+console.log(personel.name, personel.lastName); // Burak karaman
+console.log(personel["birthDate"]); // 1994
+console.log(personel.calculateAge()); // 30
+console.log(personel.hi()); // Hello
+console.log(personel.arrowFunction()); // undefined 
+console.log(personel.experiences[0]); // tester 
+console.log(personel.address.city); // potsdam 
+
+personel.experiences.forEach((work) => console.log(work))
+personel.phone = "555-34-34"
+console.log(personel);
+
+
