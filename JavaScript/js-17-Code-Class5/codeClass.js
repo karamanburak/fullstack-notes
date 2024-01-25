ebc21be5def824bee39bc0b5b7821b237cdf0cf5
burak <burak.karaman.1994@gmail.com>
2024-01-25 12:37:04 +0100

    add code challenges

--- JavaScript/js-17-Code-Class5/codeClass.js
+++ JavaScript/js-17-Code-Class5/codeClass.js
@@ -1258,3 +1258,358 @@ let products = [
 // let newProducts = products.filter((product) => product.id !== id)
 // console.log(newProducts);
 
+//! Yeni ürün eklemek
+//^ VERSION 1
+// products.push({
+
+//     "id": products.length +1,
+//     "title": "Key Holder",
+//     "description": "Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality",
+//     "price": 300,
+//     "discountPercentage": 2.92,
+//     "rating": 4.92,
+//     "stock": 500,
+//     "brand": "Golden",
+//     "category": "home-decoration",
+
+// })
+// console.log(products);
+
+//^ VERSION 2 (BETTER WAY)
+
+// let newProducts = {
+//     "id": products.length + 1,
+//     "title": "Key Holder",
+//     "description": "Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality",
+//     "price": 300,
+//     "discountPercentage": 2.92,
+//     "rating": 4.92,
+//     "stock": 500,
+//     "brand": "Golden",
+//     "category": "home-decoration",
+//     "thumbnail": "https://cdn.dummyjson.com/product-images/30/thumbnail.jpg",
+//     "images": [
+//         "https://cdn.dummyjson.com/product-images/30/1.jpg",
+//         "https://cdn.dummyjson.com/product-images/30/2.jpg",
+//         "https://cdn.dummyjson.com/product-images/30/3.jpg",
+//         "https://cdn.dummyjson.com/product-images/30/thumbnail.jpg"
+//     ]
+// }
+
+// products = [...products, newProducts]
+// console.log(products);
+
+
+//! Rest operatoru kullanarak fiyatlari indirimli fiyatlar ile güncelle - Update
+
+
+// const newProducts = products.map(product => ({
+//     ...product,
+//     price: +(product.price - (product.price * (product.discountPercentage / 100)).toFixed(2))
+// }));
+// console.log(newProducts);
+
+
+//! - Her kategoride kac ürün var göster
+
+function countCategoryElements(products) {
+    const categoryCount = {}
+    products.forEach((product) => {
+        const { category } = product
+
+        if (categoryCount[category]) {
+            categoryCount[category] += +1
+        }
+        else {
+            categoryCount[category] = 1
+        }
+    })
+
+
+    return categoryCount
+}
+
+console.log(countCategoryElements(products)); // {smartphones: 5, laptops: 5, fragrances: 5, skincare: 5, groceries: 5, …}
+
+//! kac kategori vardir
+console.log(
+    products
+        .map((product) => product.category)
+        .reduce((catArray, category) => {
+            if (!catArray.includes(category)) {
+                catArray.push(category);
+            }
+            return catArray;
+        }, [])
+        .length
+); // 6
+
+
+/* -------------------------------------------------------------------------- */
+//?                                   3.QUESTION                              */
+/* -------------------------------------------------------------------------- */
+// Bir firmanın müsterilerinin bilgilerinin bululnduğu JSON yapısı aşağıdadır.
+const customerList = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496",
+        },
+      },
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets",
+      },
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv",
+      address: {
+        street: "Victor Plains",
+        suite: "Suite 879",
+        city: "Wisokyburgh",
+        zipcode: "90566-7771",
+        geo: {
+          lat: "-43.9509",
+          lng: "-34.4618",
+        },
+      },
+      phone: "010-692-6593 x09125",
+      website: "anastasia.net",
+      company: {
+        name: "Deckow-Crist",
+        catchPhrase: "Proactive didactic contingency",
+        bs: "synergize scalable supply-chains",
+      },
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+      username: "Samantha",
+      email: "Nathan@yesenia.net",
+      address: {
+        street: "Douglas Extension",
+        suite: "Suite 847",
+        city: "McKenziehaven",
+        zipcode: "59590-4157",
+        geo: {
+          lat: "-68.6102",
+          lng: "-47.0653",
+        },
+      },
+      phone: "1-463-123-4447",
+      website: "ramiro.info",
+      company: {
+        name: "Romaguera-Jacobson",
+        catchPhrase: "Face to face bifurcated interface",
+        bs: "e-enable strategic applications",
+      },
+    },
+    {
+      id: 4,
+      name: "Patricia Lebsack",
+      username: "Karianne",
+      email: "Julianne.OConner@kory.org",
+      address: {
+        street: "Hoeger Mall",
+        suite: "Apt. 692",
+        city: "South Elvis",
+        zipcode: "53919-4257",
+        geo: {
+          lat: "29.4572",
+          lng: "-164.2990",
+        },
+      },
+      phone: "493-170-9623 x156",
+      website: "kale.biz",
+      company: {
+        name: "Robel-Corkery",
+        catchPhrase: "Multi-tiered zero tolerance productivity",
+        bs: "transition cutting-edge web services",
+      },
+    },
+    {
+      id: 5,
+      name: "Chelsey Dietrich",
+      username: "Kamren",
+      email: "Lucio_Hettinger@annie.ca",
+      address: {
+        street: "Skiles Walks",
+        suite: "Suite 351",
+        city: "Roscoeview",
+        zipcode: "33263",
+        geo: {
+          lat: "-31.8129",
+          lng: "62.5342",
+        },
+      },
+      phone: "(254)954-1289",
+      website: "demarco.info",
+      company: {
+        name: "Keebler LLC",
+        catchPhrase: "User-centric fault-tolerant solution",
+        bs: "revolutionize end-to-end systems",
+      },
+    },
+    {
+      id: 6,
+      name: "Mrs. Dennis Schulist",
+      username: "Leopoldo_Corkery",
+      email: "Karley_Dach@jasper.info",
+      address: {
+        street: "Norberto Crossing",
+        suite: "Apt. 950",
+        city: "South Christy",
+        zipcode: "23505-1337",
+        geo: {
+          lat: "-71.4197",
+          lng: "71.7478",
+        },
+      },
+      phone: "1-477-935-8478 x6430",
+      website: "ola.org",
+      company: {
+        name: "Considine-Lockman",
+        catchPhrase: "Synchronised bottom-line interface",
+        bs: "e-enable innovative applications",
+      },
+    },
+    {
+      id: 7,
+      name: "Kurtis Weissnat",
+      username: "Elwyn.Skiles",
+      email: "Telly.Hoeger@billy.biz",
+      address: {
+        street: "Rex Trail",
+        suite: "Suite 280",
+        city: "Howemouth",
+        zipcode: "58804-1099",
+        geo: {
+          lat: "24.8918",
+          lng: "21.8984",
+        },
+      },
+      phone: "210.067.6132",
+      website: "elvis.io",
+      company: {
+        name: "Johns Group",
+        catchPhrase: "Configurable multimedia task-force",
+        bs: "generate enterprise e-tailers",
+      },
+    },
+    {
+      id: 8,
+      name: "Nicholas Runolfsdottir V",
+      username: "Maxime_Nienow",
+      email: "Sherwood@rosamond.me",
+      address: {
+        street: "Ellsworth Summit",
+        suite: "Suite 729",
+        city: "Aliyaview",
+        zipcode: "45169",
+        geo: {
+          lat: "-14.3990",
+          lng: "-120.7677",
+        },
+      },
+      phone: "586.493.6943 x140",
+      website: "jacynthe.com",
+      company: {
+        name: "Abernathy Group",
+        catchPhrase: "Implemented secondary concept",
+        bs: "e-enable extensible e-tailers",
+      },
+    },
+    {
+      id: 9,
+      name: "Glenna Reichert",
+      username: "Delphine",
+      email: "Chaim_McDermott@dana.io",
+      address: {
+        street: "Dayna Park",
+        suite: "Suite 449",
+        city: "Bartholomebury",
+        zipcode: "76495-3109",
+        geo: {
+          lat: "24.6463",
+          lng: "-168.8889",
+        },
+      },
+      phone: "(775)976-6794 x41206",
+      website: "conrad.com",
+      company: {
+        name: "Yost and Sons",
+        catchPhrase: "Switchable contextually-based project",
+        bs: "aggregate real-time technologies",
+      },
+    },
+    {
+      id: 10,
+      name: "Clementina DuBuque",
+      username: "Moriah.Stanton",
+      email: "Rey.Padberg@karina.biz",
+      address: {
+        street: "Kattie Turnpike",
+        suite: "Suite 198",
+        city: "Lebsackbury",
+        zipcode: "31428-2261",
+        geo: {
+          lat: "-38.2386",
+          lng: "57.2232",
+        },
+      },
+      phone: "024-648-3804",
+      website: "ambrose.net",
+      company: {
+        name: "Hoeger LLC",
+        catchPhrase: "Centralized empowering task-force",
+        bs: "target end-to-end models",
+      },
+    },
+  ];
+  
+  //!1- Müşteri bilgilerini düzenli bir şekilde göstermek istiyor.Bunun için
+  //   İsim - adres ve telefonlarını yeni bir listeye alarak düzenli bir şekide gösterecektir.
+  
+  // Beklenen Sonuç:
+  // [
+  //     {name: 'LEANNE GRAHAM', adress: 'Gwenborough city, Kulas Light street Apt. 556 suite', phone: '1-770-736-8031 x56442'}
+  //     {name: 'ERVIN HOWELL', adress: 'Wisokyburgh city, Victor Plains street Suite 879 suite', phone: '010-692-6593 x09125'}
+  //     {name: 'CLEMENTINE BAUCH', adress: 'McKenziehaven city, Douglas Extension street Suite 847 suite', phone: '1-463-123-4447'}
+  // ]
+  
+  /* -------------------------------------------------------------------------- */
+  //!2- Müşterilerinin bazılarına telefon ile arayıp memnuniyet anketi yapacaktır
+  // Baş harfi e ile başlıyorsa Pazartesi,
+  // Baş harfi C ile başlıyorsa Çarşamba arayacaktır
+  
+  // Beklenen Sonuç:
+  // Pazartesi Aranacaklar
+  // {name: 'ERVIN HOWELL', phone: '010-692-6593 x09125'}
+  
+  // Çarşamba Aranacaklar
+  // {name: 'CLEMENTINE BAUCH', phone: '1-463-123-4447'}
+  // {name: 'CHELSEY DIETRICH', phone: '(254)954-1289'}
+  // {name: 'CLEMENTINA DUBUQUE', phone: '024-648-3804'}
+  
+  /* -------------------------------------------------------------------------- */
+  //!3- id Ad-ve email adreslerini ayrı bir listeye alıp, id numaraları tek sayı olanlara yeni çıkan elektronik cihazları ile ilgili mesaj ekleyecek, çift sayı olanlara yeni çıkan küçük ev aleti mesajını ekleyecektir
+  
+  // Beklenen Sonuc
+  // {name: 'Leanne Graham', phone: 'Sincere@april.biz', mesaj: 'Yeni çıkan XXX elektronik cihazımızı mutlaka denemelisiniz '}
+  // {name: 'Ervin Howell', phone: 'Shanna@melissa.tv', mesaj: ' Yeni Çıkan kahve yapma makinamızı deneyin.Memnun kalacaksınız'}
+  
