8fd18e62ecbdc8e216b6c7d2ecd00188b525a41a
burak <burak.karaman.1994@gmail.com>
2024-01-29 11:20:12 +0100

    update

---
+++ JavaScript/js-19-GuessNumber/game.js
@@ -0,0 +1,14 @@
+//* Pc'ye sayi tutturuldu. 1 ile 20 arasinda olabilmesi icin 20 ile carpip Math.ceil ile yukari yuvarlandi.
+
+let randomNumberPc = Math.ceil(Math.random() * 20)
+
+
+let message = document.querySelector(".msg")
+
+
+let skor = 10
+let topScore = 0
+
+skor--
+
+//! her check butonuna basinca olacaklar
