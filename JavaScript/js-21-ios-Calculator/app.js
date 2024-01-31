014b1aa307d80bb17b71f9b58d9e1769a448069e
burak <burak.karaman.1994@gmail.com>
2024-01-31 10:24:24 +0100

    update js file

--- JavaScript/js-21-ios-Calculator/app.js
+++ JavaScript/js-21-ios-Calculator/app.js
@@ -3,12 +3,19 @@
 //* ======================================================
 
 
-
-
+const numberButtons = document.querySelectorAll(".num")
+const operationButtons = document.querySelectorAll(".operator")
+const equalButton = document.querySelector(".equal")
+const previousDisplay = document.querySelector(".previous-display")
+const currentDisplay = document.querySelector(".current-display")
 
 
 
 //?operator değişkenleri
+
+let ustEkranText = ""
+let altEkranText = ""
+let islem = ""
 
 
 
@@ -20,69 +27,103 @@
 
 //?*********** herhangi bir number a basılınca
 
+numberButtons.forEach((number) => {
+    number.onclick = () => {
+        ekranaHazirlik(number.textContent)
 
+        updateEkran()
+    }
+
+
+})
 
 
 
 
 //!EKRANA HAZIRLIK
 
-  
+
+const ekranaHazirlik = (num) => {
 
 
-
-//todo kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
-
-
-
-
-
-//todo kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
-
-
-//todo kullanıcı 10 haneden sonra girmesin
+    //todo kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
 
 
 
 
-//todo kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
+
+    //todo kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
 
 
-//todo eşittir yada percent a basıldıktan sonra girilen number tek başına ekranda görünsün,çünkü yeni işlem başlıyor(ekranda 72 yazan işlem sonucu varken 5 e basınca 725 olmasın)
+    //todo kullanıcı 10 haneden sonra girmesin
 
 
 
 
-//?bütün şartları başarı ile geçtiyse basılan numaraları arka arkaya ekle
+    //todo kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
 
 
+    //todo eşittir yada percent a basıldıktan sonra girilen number tek başına ekranda görünsün,çünkü yeni işlem başlıyor(ekranda 72 yazan işlem sonucu varken 5 e basınca 725 olmasın)
+
+
+
+
+    //?bütün şartları başarı ile geçtiyse basılan numaraları arka arkaya ekle
+
+    altEkranText += num
+
+
+}
 //!BURADA YAPILANLARI EKRANA BASTIRMA
 
+const updateEkran = () => {
 
+    currentDisplay.textContent = altEkranText
 
-  //todo işlem sonucu 8 haneyi geçmesin
-
-
-
-  //?işlem girilince
+    //todo işlem sonucu 8 haneyi geçmesin
 
 
 
+    //?işlem girilince
+
+    if (islem) {
+
+        previousDisplay.textContent = `${ustEkranText}  ${islem}`
+    } else {
+        previousDisplay.textContent = ""
+    }
+
+
+
+}
 
 //?**************HERHANGİ BİR İŞLEME TIKLANDIĞINDA
 
+operationButtons.forEach((op) => {
 
-
-//?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
-//? return fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir. return den sonra else yerine if kullanmalıyız
-
-
-
-
-    //todo eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
+    op.onclick = () => {
 
 
 
+
+        //?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
+        //? return fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir. return den sonra else yerine if kullanmalıyız
+
+
+
+
+        //todo eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
+
+
+        islem = op.textContent
+
+        ustEkranText = altEkranText
+        altEkranText = ""
+        updateEkran()
+
+    }
+
+})
 
 //?**************eşittir butonuna tıklandığında
 
