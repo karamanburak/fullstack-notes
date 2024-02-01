f0dbdbf76ce4b44b90734ce132959815c5a5c04c
burak <burak.karaman.1994@gmail.com>
2024-02-01 15:01:22 +0100

    Update app.js file

--- Team Works/14-JS-toDoApp/app.js
+++ Team Works/14-JS-toDoApp/app.js
@@ -30,24 +30,50 @@ clearBtn.addEventListener("click", handleClearBtnClick);
 //&             Functions              */
 //& ---------------------------------- */
 // Every time "Enter" key is pressed => make AddTaskI clicked
-function handleKeyPress(e){
-    if(e.key === "Enter") addTaskI.click();
-}
-
-// Every time plus sign is clicked => add it as a new task
-function handleTaskClick() {
+function handleKeyPress(e) {
+    if (e.key === "Enter") addTaskI.click();
+  }
+  // Every time plus sign is clicked => add it as a new task
+  function handleAddTaskClick() {
     const inputValue = todoValueInput.value;
-
-    if(inputValue){
-        alert("PLeaser enter your task")
-        todoValueInput.focus()
-    }else {
-        // ckeck if a task with same name aldready exists.
-        const existingTask = 
+    if (inputValue === "") {
+      alert("Please enter your task");
+      todoValueInput.focus();
+    } else {
+      // check if a task with the same name already exists.
+      const existingTask = toDoList.find((task) => task.task_name === inputValue);
+      if (existingTask) {
+        alert(`Task ${inputValue} already exists! Please enter a different task`);
+        todoValueInput.focus();
+      } else {
+        // add this newly styled created task as a appendChild into li
+        const li = document.createElement("li");
+        const itemSettingDiv = document.createElement("div");
+        itemSettingDiv.className = "item-setting";
+        itemSettingDiv.textContent = inputValue;
+        const todoControlSpan = document.createElement("span");
+        const trasIcon = document.createElement("i");
+        trasIcon.className = "fa-solid fa-trash-can todo-control";
+        todoControlSpan.appendChild(trasIcon);
+        li.appendChild(itemSettingDiv);
+        li.appendChild(todoControlSpan);
+        listContainerUl.appendChild(li);
+        // push new li element to toDoList
+        toDoList.push({
+          task_name: inputValue,
+          checked: false,
+        });
+        // reset result textContent after adding task
+        resultPar.textContent = "";
+        // Update task counts (increase)
+        totalTasks++;
+        updateTaskCounts();
+        saveDate();
+      }
     }
-}
-
-
+    todoValueInput.value = ""; // reset input textContent after adding
+  }
+  
 
 
 //& ---------------------------------- */
