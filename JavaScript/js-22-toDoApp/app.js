533d6c9fd25aa39aa7a1f5427633f7dac32bf250
burak <burak.karaman.1994@gmail.com>
2024-02-02 11:38:46 +0100

    Update app.js

--- JavaScript/js-22-toDoApp/app.js
+++ JavaScript/js-22-toDoApp/app.js
@@ -39,12 +39,71 @@ addButton.onclick = () => {
         <i class="fa fa-trash fa-lg"></i>
         </li>` +listUl.innerHTML;
 
-        // <p>${todoInput.value}</p> => 2. yol
+
+        //! ekrana bastirmanin 2.yolu
+        // <p>${todoInput.value}</p> 
 
 
+        total+=1
+        totalSpan.textContent = total
 
+        todoInput.value= ""
+
+        //? Her "li" girisinde cursor inputun icinde olsun
+        todoInput.focus();
     }
 
+     //!silme metodu, ekrana basıldığı süslü de (add butonuna basilma süslüsünde func.sope) olmalı, dışarıda fa-trash lara tıklayınca, remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor.
+    //! Trash butonuna tiklaninca 
+    createDeleteButton()
 
+
+    //! chech butonuna tiklaninca 
+    createCheckButton()
+}
+
+const createDeleteButton=()=>{
+
+    document.querySelectorAll(".fa-trash").forEach((a)=> {
+        //! listeden silme
+        list.filter((todo)=>todo!=a.closest("li").querySelector("p").textContent)
+
+
+        //! ekrandan (browser dan) silme
+
+        a.onclick=()=>{
+            // a.parentElement.remove()
+             //! closest=herhangi bir elemanın istediğiniz uzaklıktaki parent ına götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanın adını yazarak direk ulaşabiliriz
+            a.closest("li").remove()
+            total = total - 1;
+            totalSpan.textContent=total
+        }
+    })
+}
+
+const createCheckButton=()=>{
+
+document.querySelectorAll("fa-check").forEach(
+    (a)=> 
+(a.onclick=()=>{
+    // if(a.parentElement.className=="checked"){
+    //     a.parentElement.className=""
+    // }else{
+    //     a.parentElement.className="checked"
+    // }
+
+    if(a.parentElement.classList.contains("checked")){
+        a.parentElement.classList.remove("checked")
+        completed-=1
+    }else{
+        a.parentElement.classList.add("checked");
+        completed+=1
+
+    }
+    completeSpan.textContent=completed
+})
+)
 
 }
+
+// !! classList ve className sonuç istendiğinde aynı sonucu verir tek farkla; classList bir liste, className bir isim şeklinde verir. bunun tek dezavantajı, zaten class ı olan bir elemana className="örnek" şeklinde eleman atamak istersek, varolan elemanları silip sadece örnek class ı nı atar. classList ile toggle ve contains kullanmalıyız, className ile toggle kullanamıyoruz , className de contains yerine includes kullanmalıyız, javascriptte includes tercih edilir
