f19a8b1cc1f75e53d2527c13b01ff6c4e31c209d
burak <burak.karaman.1994@gmail.com>
2024-02-02 11:53:44 +0100

    Update app.js

--- JavaScript/js-22-toDoApp/app.js
+++ JavaScript/js-22-toDoApp/app.js
@@ -15,45 +15,45 @@ const totalSpan = document.querySelector("#toplam")
 const completeSpan = document.querySelector("#tamamlanan")
 
 addButton.onclick = () => {
-// input bos iken
-    if(!todoInput.value){
+    // input bos iken
+    if (!todoInput.value) {
 
         alert("Please enter a Note")
         // Daha önce listeye eklenen not bir daha eklenmesin.
-    } else if(list.includes(todoInput.value)){
+    } else if (list.includes(todoInput.value)) {
         // listUl.textContent.includes(todoInput.value)
 
         return
         // return=if in içindeki şart true ise hiçbirşey yapma, koda alt satırdan devam et demek
 
-    } else{
+    } else {
 
         list.push(todoInput.value)
 
 
         //! Ekranda listeyi göster
-        listUl.innerHTML= `
+        listUl.innerHTML = `
         <li class="empty">
         <i class="fa fa-check"></i>
-        <p>${list[list.length-1]}</p>
+        <p>${list[list.length - 1]}</p>
         <i class="fa fa-trash fa-lg"></i>
-        </li>` +listUl.innerHTML;
+        </li>` + listUl.innerHTML;
 
 
         //! ekrana bastirmanin 2.yolu
         // <p>${todoInput.value}</p> 
 
 
-        total+=1
+        total += 1
         totalSpan.textContent = total
 
-        todoInput.value= ""
+        todoInput.value = ""
 
         //? Her "li" girisinde cursor inputun icinde olsun
         todoInput.focus();
     }
 
-     //!silme metodu, ekrana basıldığı süslü de (add butonuna basilma süslüsünde func.sope) olmalı, dışarıda fa-trash lara tıklayınca, remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor.
+    //!silme metodu, ekrana basıldığı süslü de (add butonuna basilma süslüsünde func.sope) olmalı, dışarıda fa-trash lara tıklayınca, remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor.
     //! Trash butonuna tiklaninca 
     createDeleteButton()
 
@@ -62,47 +62,53 @@ addButton.onclick = () => {
     createCheckButton()
 }
 
-const createDeleteButton=()=>{
+const createDeleteButton = () => {
 
-    document.querySelectorAll(".fa-trash").forEach((a)=> {
+    document.querySelectorAll(".fa-trash").forEach((a) => {
         //! listeden silme
-        list.filter((todo)=>todo!=a.closest("li").querySelector("p").textContent)
+        list.filter((todo) => todo != a.closest("li").querySelector("p").textContent)
 
 
         //! ekrandan (browser dan) silme
 
-        a.onclick=()=>{
+        a.onclick = () => {
             // a.parentElement.remove()
-             //! closest=herhangi bir elemanın istediğiniz uzaklıktaki parent ına götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanın adını yazarak direk ulaşabiliriz
+            //! closest=herhangi bir elemanın istediğiniz uzaklıktaki parent ına götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanın adını yazarak direk ulaşabiliriz
             a.closest("li").remove()
             total = total - 1;
-            totalSpan.textContent=total
+            totalSpan.textContent = total
+
+            //! eğer silinen işin classname inde checked varsa (bitirilmiş yapılmış bir işse) completed sayısından da bir eksiltme yap
+            if (a.parentElement.classList.contains("checked")) {
+                completed -= 1
+                completeSpan.textContent = completed
+            }
+
         }
     })
 }
 
-const createCheckButton=()=>{
+const createCheckButton = () => {
+    document.querySelectorAll("fa-check").forEach(
+        (a) =>
+        (a.onclick = () => {
+            // if(a.parentElement.className=="checked"){
+            //     a.parentElement.className=""
+            // }else{
+            //     a.parentElement.className="checked"
+            // }
 
-document.querySelectorAll("fa-check").forEach(
-    (a)=> 
-(a.onclick=()=>{
-    // if(a.parentElement.className=="checked"){
-    //     a.parentElement.className=""
-    // }else{
-    //     a.parentElement.className="checked"
-    // }
+            if (a.parentElement.classList.contains("checked")) {
+                a.parentElement.classList.remove("checked")
+                completed -= 1
+            } else {
+                a.parentElement.classList.add("checked");
+                completed += 1
 
-    if(a.parentElement.classList.contains("checked")){
-        a.parentElement.classList.remove("checked")
-        completed-=1
-    }else{
-        a.parentElement.classList.add("checked");
-        completed+=1
-
-    }
-    completeSpan.textContent=completed
-})
-)
+            }
+            completeSpan.textContent = completed
+        })
+    )
 
 }
 
