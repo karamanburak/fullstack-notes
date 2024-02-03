41cced7d08499c02d1669d30165cf44395095209
burak <karaman.buraak@gmail.com>
2024-02-03 08:47:23 +0100

    Update index.html, app.js, localStorage.js

--- JavaScript/js-22-toDoApp/app.js
+++ JavaScript/js-22-toDoApp/app.js
@@ -1,115 +1,113 @@
-//! Local Storage siz yol
-
-let list = []
+//!LOCALSTORAGE SİZ YOL
+let liste = [];
 
 //! boş liste hem database işini kolaylaştırır, hem ul ye eklenen verinin aynısı tekrar eklenmesin demek için ,bütün p leri çağır içeriğinde input.value deki değer varsa ekleme, demeli ama p oluşmadan bunu diyemeyiz, bu şekilde liste dizisine eklenen veri globalde old için içeriğine (includes) her yerden bakılabilir
-
-
 let total = 0;
 let completed = 0;
 
-const addButton = document.querySelector("#todo-button")
-const listUl = document.querySelector("#todo-ul")
-const todoInput = document.querySelector("#todo-input")
-const totalSpan = document.querySelector("#toplam")
-const completeSpan = document.querySelector("#tamamlanan")
+const addButton = document.querySelector("#todo-button");
+const listUl = document.querySelector("#todo-ul");
+const todoInput = document.querySelector("#todo-input");
+const totalSpan = document.querySelector("#toplam");
+const completeSpan = document.querySelector("#tamamlanan");
 
 addButton.onclick = () => {
-    // input bos iken
-    if (!todoInput.value) {
+  //input boşken
+  if (!todoInput.value) {
+    alert("lütfen bir not giriniz");
 
-        alert("Please enter a Note")
-        // Daha önce listeye eklenen not bir daha eklenmesin.
-    } else if (list.includes(todoInput.value)) {
-        // listUl.textContent.includes(todoInput.value)
+    //daha önce listeye eklenen not bir daha eklenmesin
+  } else if (liste.includes(todoInput.value)) {
+    // listUl.textContent.includes(todoInput.value)
 
-        return
-        // return=if in içindeki şart true ise hiçbirşey yapma, koda alt satırdan devam et demek
+    return;
+    // return=if in içindeki şart true ise hiçbirşey yapma, koda alt satırdan devam et demek
+  } else {
+    liste.push(todoInput.value);
 
-    } else {
-
-        list.push(todoInput.value)
-
-
-        //! Ekranda listeyi göster
-        listUl.innerHTML = `
-        <li class="empty">
-        <i class="fa fa-check"></i>
-        <p>${list[list.length - 1]}</p>
-        <i class="fa fa-trash fa-lg"></i>
-        </li>` + listUl.innerHTML;
+    //!ekranda listeyi göster
+    listUl.innerHTML =
+      `
+<li class="ayse">
+<i class="fa fa-check fa-lg"></i>
 
 
-        //! ekrana bastirmanin 2.yolu
-        // <p>${todoInput.value}</p> 
+<p>${liste[liste.length - 1]}</p>
 
 
-        total += 1
-        totalSpan.textContent = total
+<i class="fa fa-trash fa-lg"></i>
+</li>` + listUl.innerHTML;
 
-        todoInput.value = ""
+    //!ekrana bastırmanın 2.yolu
+    //<p>${todoInput.value}</p>;
 
-        //? Her "li" girisinde cursor inputun icinde olsun
-        todoInput.focus();
-    }
+    total += 1;
+    totalSpan.textContent = total;
 
-    //!silme metodu, ekrana basıldığı süslü de (add butonuna basilma süslüsünde func.sope) olmalı, dışarıda fa-trash lara tıklayınca, remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor.
-    //! Trash butonuna tiklaninca 
-    createDeleteButton()
+    todoInput.value = "";
 
+    //? her li girişinde cursor input un içinde olsun
+    todoInput.focus();
+  }
 
-    //! chech butonuna tiklaninca 
-    createCheckButton()
+  //!silme metodu, ekrana basıldığı süslü de (add butonuna basılma süslüsünde, func.scope)  olmalı, dışarıda fa-trash lara tıklayınca, remove mantıklı değil, ortada fa-trash daha oluşmadan (add butonuna basılmadan) js ye çağırmak gibi oluyor
+  //!trash butonuna tıklanınca
+  createDeleteButon();
+
+  //!check butonuna tıklanınca
+  createCheckButon();
+};
+
+const createDeleteButon = () => {
+  document.querySelectorAll(".fa-trash").forEach((a) => {
+    a.onclick = () => {
+      //!listeden silme
+
+      liste = liste.filter(
+        (todo) => todo != a.closest("li").querySelector("p").textContent
+      );
+
+      //!ekrandan (browser dan silme)
+      // a.parentElement.remove()
+      //! closest=herhangi bir elemanın istediğiniz uzaklıktaki parent ına götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanın adını yazarak direk ulaşabiliriz
+      a.closest("li").remove();
+      total = total - 1;
+      totalSpan.textContent = total;
+
+      //! eğer silinen işin classList inde checked varsa (bitirilmiş yapılmış bir işse) completed sayısından da bir eksiltme yap
+if(a.parentElement.classList.contains("checked")){
+
+    completed-=1
+    completeSpan.textContent=completed
 }
 
-const createDeleteButton = () => {
 
-    document.querySelectorAll(".fa-trash").forEach((a) => {
-        //! listeden silme
-        list.filter((todo) => todo != a.closest("li").querySelector("p").textContent)
+    };
+  });
+};
 
+const createCheckButon = () => {
+  document.querySelectorAll(".fa-check").forEach(
+    (a) =>
+      (a.onclick = () => {
+        // if(a.parentElement.className=="checked"){
+        //     a.parentElement.className=""
+        // }else{
+        //     a.parentElement.className="checked"
+        // }
 
-        //! ekrandan (browser dan) silme
+        if (a.parentElement.classList.contains("checked")) {
 
-        a.onclick = () => {
-            // a.parentElement.remove()
-            //! closest=herhangi bir elemanın istediğiniz uzaklıktaki parent ına götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanın adını yazarak direk ulaşabiliriz
-            a.closest("li").remove()
-            total = total - 1;
-            totalSpan.textContent = total
+          a.parentElement.classList.remove("checked");
+         completed-=1
 
-            //! eğer silinen işin classname inde checked varsa (bitirilmiş yapılmış bir işse) completed sayısından da bir eksiltme yap
-            if (a.parentElement.classList.contains("checked")) {
-                completed -= 1
-                completeSpan.textContent = completed
-            }
+        } else {
+          a.parentElement.classList.add("checked");
+          completed+=1
 
         }
-    })
-}
-
-const createCheckButton = () => {
-    document.querySelectorAll("fa-check").forEach(
-        (a) =>
-        (a.onclick = () => {
-            // if(a.parentElement.className=="checked"){
-            //     a.parentElement.className=""
-            // }else{
-            //     a.parentElement.className="checked"
-            // }
-
-            if (a.parentElement.classList.contains("checked")) {
-                a.parentElement.classList.remove("checked")
-                completed -= 1
-            } else {
-                a.parentElement.classList.add("checked");
-                completed += 1
-
-            }
-            completeSpan.textContent = completed
-        })
-    )
-
-}
-
+        completeSpan.textContent=completed
+      })
+  );
+};
 // !! classList ve className sonuç istendiğinde aynı sonucu verir tek farkla; classList bir liste, className bir isim şeklinde verir. bunun tek dezavantajı, zaten class ı olan bir elemana className="örnek" şeklinde eleman atamak istersek, varolan elemanları silip sadece örnek class ı nı atar. classList ile toggle ve contains kullanmalıyız, className ile toggle kullanamıyoruz , className de contains yerine includes kullanmalıyız, javascriptte includes tercih edilir
--- JavaScript/js-22-toDoApp/localStorage.js
+++ JavaScript/js-22-toDoApp/localStorage.js
@@ -1,112 +1,124 @@
 
-let list = []
+// short circuit
+let liste = JSON.parse(localStorage.getItem("LİSTE")) || []
+ 
+// let liste = JSON.parse(localStorage.getItem("LİSTE")) ? JSON.parse(localStorage.getItem("LİSTE")):[]
 
 
 
 
-let total = 0;
-let completed = 0;
+const addButton = document.querySelector("#todo-button");
+const listUl = document.querySelector("#todo-ul");
+const todoInput = document.querySelector("#todo-input");
+const totalSpan = document.querySelector("#toplam");
+const completeSpan = document.querySelector("#tamamlanan");
 
-const addButton = document.querySelector("#todo-button")
-const listUl = document.querySelector("#todo-ul")
-const todoInput = document.querySelector("#todo-input")
-const totalSpan = document.querySelector("#toplam")
-const completeSpan = document.querySelector("#tamamlanan")
 
+//!add butonuna tıklandığında
 addButton.onclick = () => {
-    // input bos iken
-    if (!todoInput.value) {
+  //input boşken
+  if (!todoInput.value) {
+    alert("lütfen bir not giriniz");
 
-        alert("Please enter a Note")
-        // Daha önce listeye eklenen not bir daha eklenmesin.
-    } else if (list.includes(todoInput.value)) {
-        // listUl.textContent.includes(todoInput.value)
+    //daha önce listeye eklenen not bir daha eklenmesin
+  } else if (liste.includes(todoInput.value)) {
 
-        return
-        // return=if in içindeki şart true ise hiçbirşey yapma, koda alt satırdan devam et demek
-
-    } else {
-
-        list.push(todoInput.value)
+    return;
+  
+  } else {
+    liste.push(todoInput.value);
 
 
-        //! Ekranda listeyi göster
-        listUl.innerHTML = `
-        <li class="empty">
-        <i class="fa fa-check"></i>
-        <p>${list[list.length - 1]}</p>
-        <i class="fa fa-trash fa-lg"></i>
-        </li>` + listUl.innerHTML;
+
+    //!listeye eleman eklenince localStorage deki LİSTE yi güncelle
+
+    localStorage.setItem("LİSTE", JSON.stringify(liste))
+
+    //!ekranda listeyi göster
+   showList()
+
+   
+
+    todoInput.value = "";
+
+   
+  }
+ 
+};
 
 
-        //! ekrana bastirmanin 2.yolu
-        // <p>${todoInput.value}</p> 
+const showList=()=>{
+
+    //!liste kayıtlı yerden gelip UI a bastırılacak, UI da varolan tekrar basılmış olmasın diye, önce ekranı temizledik
+   listUl.textContent=""
+
+liste.forEach((todo)=>{
+
+listUl.innerHTML =
+  `
+<li class="ayse">
+<i class="fa fa-check fa-lg"></i>
 
 
-        total += 1
-        totalSpan.textContent = total
+<p>${todo}</p>
 
-        todoInput.value = ""
 
-        //? Her "li" girisinde cursor inputun icinde olsun
-        todoInput.focus();
-    }
+<i class="fa fa-trash fa-lg"></i>
+</li>` + listUl.innerHTML;
 
+})
+
+   
+
+
+
+createDeleteButon()
+createCheckButon()
+
+
+
+}
+
+
+
+
+const createDeleteButon = () => {
+  document.querySelectorAll(".fa-trash").forEach((a) => {
+    a.onclick = () => {
+      //!listeden silme
+
+      liste = liste.filter(
+        (todo) => todo != a.closest("li").querySelector("p").textContent
+      );
+
+      a.closest("li").remove();
     
-    createDeleteButton()
 
+      localStorage.setItem("LİSTE", JSON.stringify(liste))
+    
+    };
+  });
+};
 
-    //! chech butonuna tiklaninca 
-    createCheckButton()
-}
+const createCheckButon = () => {
+  document.querySelectorAll(".fa-check").forEach(
+    (a) =>
+      (a.onclick = () => {
+      
 
-const createDeleteButton = () => {
-
-    document.querySelectorAll(".fa-trash").forEach((a) => {
-        //! listeden silme
-        list.filter((todo) => todo != a.closest("li").querySelector("p").textContent)
-
-
-        //! ekrandan (browser dan) silme
-
-        a.onclick = () => {
-            // a.parentElement.remove()
-            //! closest=herhangi bir elemanın istediğiniz uzaklıktaki parent ına götürür. yani a.parentElement.parentElement.parentElement ile 3. babaya ulaşabilirken closest ile 3. babanın adını yazarak direk ulaşabiliriz
-            a.closest("li").remove()
-            total = total - 1;
-            totalSpan.textContent = total
-
-            //! eğer silinen işin classname inde checked varsa (bitirilmiş yapılmış bir işse) completed sayısından da bir eksiltme yap
-            if (a.parentElement.classList.contains("checked")) {
-                completed -= 1
-                completeSpan.textContent = completed
-            }
-
+        if (a.parentElement.classList.contains("checked")) {
+          a.parentElement.classList.remove("checked");
+        
+        } else {
+          a.parentElement.classList.add("checked");
+        
         }
-    })
-}
+      
+      })
+  );
+};
 
-const createCheckButton = () => {
-    document.querySelectorAll("fa-check").forEach(
-        (a) =>
-        (a.onclick = () => {
-            // if(a.parentElement.className=="checked"){
-            //     a.parentElement.className=""
-            // }else{
-            //     a.parentElement.className="checked"
-            // }
 
-            if (a.parentElement.classList.contains("checked")) {
-                a.parentElement.classList.remove("checked")
-                completed -= 1
-            } else {
-                a.parentElement.classList.add("checked");
-                completed += 1
 
-            }
-            completeSpan.textContent = completed
-        })
-    )
-
-}
-
+//!ekran ilk açıldığında localstorage dekileri getir
+ showList()
