749911da753200d0e05d6ebde08ce9c1e3d505ea
Burak Karaman <150926922+karamanburak@users.noreply.github.com>
2024-02-14 10:16:20 +0100

    Update app.js

--- JavaScript/js-25-Async-Project1/app.js
+++ JavaScript/js-25-Async-Project1/app.js
@@ -1,1 +1,24 @@
+const mealsDiv = document.querySelector(".food")
+
+
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=").then((response) => response.json()).then((data)=>showScreen(data.meals)).catch((err)=>console.log(err));
+
+function showScreen (data) {
+
+data.forEach((meal)=>{
+
+mealsDiv.innerHTML += `
+
+<div class="col-md-3 m-1">
+
+<p>${meal.strMeal}</p>
+<img style = width:200px src="${meal.strMealThumb}"/>
+</div>
+
+
+`
+
+})
+
+}
 
