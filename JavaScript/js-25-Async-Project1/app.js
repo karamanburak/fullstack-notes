fccfcfb2d6be935f3cf184ed78d8f09b18417407
Burak Karaman <150926922+karamanburak@users.noreply.github.com>
2024-02-14 11:17:19 +0100

    Update app.js

--- JavaScript/js-25-Async-Project1/app.js
+++ JavaScript/js-25-Async-Project1/app.js
@@ -1,13 +1,26 @@
-const mealsDiv = document.querySelector(".food")
+let showList = []; //!boş bir array aç. alttaki arama olayında datayı görebilmek için global e boş bir array açıp, fetch ile gelen data yı bu boş array e attık
+
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+.then((response) => response.json())
+.then((data) =>
+{showList=data
+
+    showScreen(showList.meals)}
+    
+    )
+
+.catch((err) => console.log(err));
 
 
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=").then((response) => response.json()).then((data)=>showScreen(data.meals)).catch((err)=>console.log(err));
+//! EKRANA BASTIRMA METODU
+function showScreen(data) {
+    const mealsDiv = document.querySelector(".food")
+    
+    mealsDiv.innerHTML =""
 
-function showScreen (data) {
+    data.forEach((meal) => {
 
-data.forEach((meal)=>{
-
-mealsDiv.innerHTML += `
+        mealsDiv.innerHTML += `
 
 <div class="col-md-3 m-1">
 
@@ -15,10 +28,29 @@ mealsDiv.innerHTML += `
 <img style = width:200px src="${meal.strMealThumb}"/>
 </div>
 
-
 `
+    });
 
-})
+};
+
+//! bayraklara tiklaninca o bayragin ülkesinin yemekleri gelsin
+
+document.querySelectorAll("img").forEach(
+    (country) =>
+    (country.onclick = () => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country.id}`).then((response)=> response.json()).then((data)=>showScreen(data.meals));
+
+    })
+);
+
+
+//!arama inputuna yazdığım harfleri içeren yemekleri ekrana bastır (oninput=kullanıcı input elemanına her veri girişinde çalışmaktadır)
+
+document.querySelector("#call").oninput=(e)=> {
+
+
+let data = showList.meals.filter((item)=>item.strMeal.toLowerCase().includes(e.target.value.toLowerCase()))
+
+showScreen(data)
 
 }
-
