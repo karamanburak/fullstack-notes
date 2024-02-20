0a1097bdef24ed8e4543e063fe1d3fb6f2e677a0
Burak Karaman <150926922+karamanburak@users.noreply.github.com>
2024-02-20 10:14:53 +0100

    Create index.js

---
+++ JavaScript/js-27-Module/modules/index.js
@@ -0,0 +1,13 @@
+console.log("index js den selamlar");
+
+import ugurla, {CALISMA_SAATI, decrement, increment, myName, topla} from "./myModule.js";
+
+// import ugurla from "./myModule.js"
+console.log(CALISMA_SAATI);
+
+ugurla()
+
+
+console.log(topla(3,5));
+
+decrement(7)
