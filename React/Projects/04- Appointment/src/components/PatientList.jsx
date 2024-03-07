5f8cee060613a8373b6c1b3b1f371dee4bfcff66
burak <karaman.buraak@gmail.com>
2024-03-07 13:13:32 +0100

    Update project

--- React/Projects/04- Appointment/src/components/PatientList.jsx
+++ React/Projects/04- Appointment/src/components/PatientList.jsx
@@ -8,21 +8,29 @@ const PatientList = ({ hastalar, setHastalar, doctors }) => {
     <div>
       {hastalar.map((patient) => (
         <div>
+           {/* //!2.yolda acik olacak kisim */}
           {doctors.map((dr) => (
-            dr.doctorName == patient.myDoctor && (<div className={patient.isDone ? "trueStil" : "falseStyle"}
-              onDoubleClick={() => setHastalar(hastalar.map((hst) => hst.id === patient.id ? { ...patient, isDone: !hst.isDone } : hst))}
-            >
-              <div>
-                <h2>{patient.patientName} </h2>
-                <h4>{patient.day} </h4>
-                <h3>{patient.myDoctor}</h3>
-              </div>
-              <div>
-                <FaTimesCircle onClick={() => setHastalar(hastalar.filter((a) => a.id !== patient.id))} style={{ color: "red" }} />
-              </div>
-            </div>)
-          ))}
+            dr.doctorName == patient.myDoctor && (
+              <div
+                className={patient.isDone ? "trueStil" : "falseStyle"}
+                onDoubleClick={() =>
+                  setHastalar(hastalar.map((hst) =>
+                    hst.id === patient.id
+                      ? { ...patient, isDone: !hst.isDone }
+                      : hst))}>
+                <div>
+                  <h2>{patient.patientName} </h2>
+                  <h4>{patient.day} </h4>
+                  <h3>{patient.myDoctor}</h3>
+                </div>
 
+                <FaTimesCircle
+                  onClick={() =>
+                    setHastalar(hastalar.filter((a) => a.id !== patient.id))}
+                  style={{ color: "red" }} />
+
+              </div>
+            )))}
 
         </div>
       ))}
--- React/Projects/04- Appointment/src/pages/Home.jsx
+++ React/Projects/04- Appointment/src/pages/Home.jsx
@@ -7,14 +7,30 @@ const Home = () => {
 
 const [doctors, setDoctors] = useState(doctorData)
 const [hastalar, setHastalar] = useState(hastaData)
-
 const [show,setShow] = useState(true)
 
 const doctorClick = (idD) => {
 
-  setShow(!show)
-  setDoctors(show ? 
-    doctors.filter((a)=> a.id===idD): doctorData)
+
+  //!1.yol
+  // if (doctors.length === 4) {
+  //   setShow(false);
+  //   doctors = doctors.filter((a) => a.id === idD);
+  //   setDoctors(doctors);
+
+  //   setHastalar(hastalar.filter((a) => a.myDoctor === doctors[0].doctorName));
+  // } else {
+  //   setShow(true);
+
+  //   setDoctors(doctorData);
+  //   setHastalar(hastaData);
+  // }
+
+  //!2.yol
+  setShow(!show);
+
+  // //doktorlar dizisini tıklanan doktorla güncelle
+  setDoctors(show ? doctors.filter((i) => i.id === idD) : doctorData);
 
 }
 
@@ -30,12 +46,19 @@ const doctorClick = (idD) => {
   {doctors.map((dr)=>{
     return(
       <div key={dr.id} >
-        <img src={dr.doctorImg} width="180px" height="150px" 
+        <img 
+        src={dr.doctorImg}
+         width="180px"
+          height="150px" 
           className="btn"
           style={{background: show ? "aqua" : "lightgreen"}}
           onClick={()=> doctorClick(dr.id)}
         />
-        <h4 style={{background:show ? "aqua" : "lightgreen", borderLeft: `10px solid ${show ? "blue" : "green"}`}}>{dr.doctorName}</h4>
+        <h4 
+        style={{background:show ? "aqua" : "lightgreen", 
+        borderLeft: `10px solid ${show ? "blue" : "green"}`}}
+        >
+        {dr.doctorName}</h4>
       </div>
 
     )
