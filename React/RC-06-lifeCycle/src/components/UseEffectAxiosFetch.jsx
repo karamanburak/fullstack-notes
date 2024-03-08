6701fb11435a94db5c1968b11578cf99dc994595
burak <karaman.buraak@gmail.com>
2024-03-08 12:01:09 +0100

    Update UseEffectAxiosFetch.jsx

--- React/RC-06-lifeCycle/src/components/UseEffectAxiosFetch.jsx
+++ React/RC-06-lifeCycle/src/components/UseEffectAxiosFetch.jsx
@@ -3,22 +3,46 @@ import React, { useEffect, useState } from 'react'
 export const UseEffectAxiosFetch = () => {
   const [people, setPeople] = useState([])
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json()).then((data) => setPeople(data))
-  }, [])
+  //^1.yol fetch then (chain)\\
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json()).then((data) => setPeople(data))
+  // }, [])
   
+  //^2.yol fetch async await\\
+
+    const getData = async () => {
+
+   const res = await fetch("https://jsonplaceholder.typicode.com/users")
+     const data = await res.json()
+
+     setPeople(data)
+    }
+
+    useEffect(() => {
+      getData()
+  
+    }, [])
+    
+
 
   return (
-    <div>
-      {people.map((a)=>{
+    <div className='container text-center mt-4'>
+    <div className='row'>
+      {people.map(({id,username,phone,name})=>{
         return(
-          <div>
-            <h1> 
-            {a.name}
-            </h1>
+          <div key={id} className='col-12 col-sm-6 col-md-4'>
+            {/* https://api.dicebear.com/7.x/avataaars/svg?seed=Felix */}
+            <img src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${name}`} />
+            <h5> 
+            {name}
+            </h5>
+            <h6>{username} </h6>
+            <h6>{phone} </h6>
           </div>
         )
       })}
+      </div>
     </div>
   )
 }
