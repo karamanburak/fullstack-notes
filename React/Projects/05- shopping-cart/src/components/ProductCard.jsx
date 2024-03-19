606fd012c2bce096a5f408935856a7b7ce2e6933
Burak Karaman <karaman.buraak@gmail.com>
2024-03-19 11:32:35 +0100

    Update project & Add new functions

--- React/Projects/05- shopping-cart/src/components/ProductCard.jsx
+++ React/Projects/05- shopping-cart/src/components/ProductCard.jsx
@@ -2,18 +2,40 @@ import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 // parseFloat; komutu tam sayıyı virgüllü sayıya çevirir
-const ProductCard = () => {
- 
+const ProductCard = ({product,getData}) => {
+  //^ parseFloat= kullanici tam sayi bile girse virgüllü bir sayi olarak cevirir, ekrana öyle basar\\
+  const {id,image, price, name, dampingRate, amount} = product
+  const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
 
+ //bu component her data icin ayri basildigindan ayrica id parametresi yollamaya gerek yok..\\
+  const handleRemove = async () => {
+    await axios.delete(`${BASE_URL}/${id}`)
+    getData()
 
+  }
 
+  const handleMinus = async () => {
+
+    if (amount > 1) {
+      {await axios.put(`${BASE_URL}/${id}`, { ...product, amount: amount - 1 });
+      getData()}
+    }
+    else {
+      handleRemove()
+    }
+  }
+  const handlePlus = async () => {
+    await axios.put(`${BASE_URL}/${id}`, { ...product, amount: +(amount) + 1 });
+    getData()
+
+  };
 
   return (
     <div className="card shadow-lg mb-3">
       <div className="row g-0">
         <div className="col-md-5">
           <img
-            src={""}
+            src={image}
             className="w-100 h-100 rounded-start"
             alt={"name"}
             title={""}
@@ -23,33 +45,32 @@ const ProductCard = () => {
           <div className="card-body">
             <h5
               className="card-title"
-              role="button"
-          
-            >
-            {}
+              role="button" >
+            {name}
             </h5>
             <div className="product-price d-flex flex-wrap align-items-center">
-              <span className="damping-price text-warning h2">
-                $ 
+              <span className="text-warning h2">
+                $ {(price*dampingRate).toFixed(2)}
               </span>
               <span className="h5 text-dark ms-2 text-decoration-line-through">
-          
+            {parseFloat(price).toFixed(2)}
               </span>
             </div>
             <div className="border border-1 border-dark shadow-lg d-flex justify-content-center p-2">
-              <div className="quantity-controller">
+              <div>
                 <button
                   className="btn btn-secondary btn-sm"
-          
+                  onClick={handleMinus}
                 >
                   <i className="fas fa-minus"></i>
                 </button>
                 <p className="d-inline mx-4" id="product-quantity">
-               {}
+               {amount}
                 </p>
                 <button
                   className="btn btn-secondary btn-sm"
-             
+                  onClick={handlePlus}
+
                 >
                   <i className="fas fa-plus"></i>
                 </button>
@@ -57,16 +78,16 @@ const ProductCard = () => {
             </div>
             <div className="product-removal mt-4">
               <button
-                className="btn btn-danger btn-sm w-100 remove-product"
-               
+                className="btn btn-danger btn-sm w-100"
+               onClick={handleRemove}
               >
                 <i className="fa-solid fa-trash-can me-2"></i>Remove
               </button>
             </div>
             <div className="mt-2">
-              Product Total: $
+              Product Total: $ 
               <span className="product-line-price">
-            
+              {(price*dampingRate*amount).toFixed(2)}
               </span>
             </div>
           </div>
--- React/Projects/05- shopping-cart/src/pages/ProductList.jsx
+++ React/Projects/05- shopping-cart/src/pages/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import axios from "axios";
 
@@ -6,29 +6,47 @@ import axios from "axios";
 const ProductList = () => {
 const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
 
+const [produkte,setProdukte] = useState([])
+const  [error, setError] = useState(false)
+const  [loading, setLoading] = useState(true)
+
 const getData = async () => {
 
- const {data} = await axios.get(BASE_URL)
- console.log(data);
+  try {
+    const { data } = await axios.get(BASE_URL)
+    setProdukte(data)
 
+  } catch (err) {
+    setError(true)
+    console.log(err);
+
+  }finally{
+    setLoading(false)
+  }
 }
 useEffect(()=>{
   getData()
 },[])
 
+if(error){
+  return <p>Something went wrong...</p>
+}
+
+
   return (
     <div className="container mt-3">
       <div className="d-sm-block d-md-flex">
-        (
+      {loading ? <p>Loading...</p> :   (
         <>
           <article id="product-panel" className="col-md-6">
-            {[].map((product) => (
-              <ProductCard key={product.id} />
+            {produkte.map((product) => (
+              <ProductCard key={product.id} product={product} getData={getData}/>
             ))}
           </article>
 
         </>
-        )
+        )}
+      
       </div>
     </div>
   );
