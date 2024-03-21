7f45dc39dd38d11c948036586e399bfad9f69ced
Burak Karaman <karaman.buraak@gmail.com>
2024-03-22 00:24:25 +0100

    Add consuming method (context)

--- React/RC-09-Context-Reducer/context-2-best2/src/context/KullaniciProvider.jsx
+++ React/RC-09-Context-Reducer/context-2-best2/src/context/KullaniciProvider.jsx
@@ -1,26 +1,38 @@
-import React, { useContext } from "react";
 import { createContext } from "react";
-
-
+import { useContext } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
+//!1-Create Context\\
+export const KullaniciContext = createContext()
 
+//!provider\\
+const KullaniciProvider = ({ children }) => {
+  const [users, setUsers] = useState([])
 
-const KullaniciProvider = () => {
+  useEffect(() => {
+    fetch("https://api.github.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
 
+  }, [])
+
+  const changeWidth = (id, size) => {
+    setUsers(users.map((a) => a.id === id ? { ...a, width: size } : a))
+  }
 
 
   return (
-<div></div>
-  );
-
-
-
-  
-
+    <KullaniciContext.Provider value={{ users, changeWidth }}>
+      {children}
+    </KullaniciContext.Provider>
+  ); 
 };
 
 
+//!Consuming burada yaparsak ihtiyaç duyan child larda bir daha çağırmıyoruz (opsiyonel tercih etmeyebilir, eski yoldan yani verileri çağıran sayfalara useContext yazarak devam edebilirsiniz.)
+export const useKullaniciCntx = () => {
+  return useContext(KullaniciContext)
+}
 
 export default KullaniciProvider;
