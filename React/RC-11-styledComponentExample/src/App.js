b336277cb58f7a7d3db0028d60a0b534926c0f9e
Burak Karaman <karaman.buraak@gmail.com>
2024-03-22 10:35:56 +0100

    Create styled-components example project

---
+++ React/RC-11-styledComponentExample/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Card from "./components/Card";
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
