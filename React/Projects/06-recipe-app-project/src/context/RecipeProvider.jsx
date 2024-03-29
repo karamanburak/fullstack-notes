19465fb0acfea565b7d2972ef91927f429b53adf
Burak Karaman <karaman.buraak@gmail.com>
2024-03-29 14:51:14 +0100

    Update project & Add reducer

--- React/Projects/06-recipe-app-project/src/context/RecipeProvider.jsx
+++ React/Projects/06-recipe-app-project/src/context/RecipeProvider.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useState } from 'react'
 
 //!context alani create edildi \\
 export const RecipeContext = createContext()
@@ -15,20 +15,34 @@ const RecipeProvider = ({children}) => {
 
     //! Home, header ve recipeCard sayfalari icin \\ 
     const [recipes,setRecipes] = useState ([])
+    const [loading,setLoading] = useState(false)
+    const [error,setError] = useState(false)
     const [query,setQuery]  = useState("")
     const [mealType, setMealType] = useState("Breakfast")
 
     const BASE_URL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${mealType}`
 
     const getData = async () => {
-     const {data} = await axios.get(BASE_URL)
-     console.log(data.hits);
+      setLoading(true)
+      try {
+           const {data} = await axios.get(BASE_URL)
+    //  console.log(data.hits);
      setRecipes(data.hits)
+      } catch (error) {
+        setError(true)
+      }finally {
+        setLoading(false)
+      }
+  
     }
 
-    useEffect(()=>{
-        getData()
-    }, [])
+  if (error) {
+    return <p>Something went wrong...</p>
+  }
+  if(loading){
+    return <p>Loading...</p>
+  }
+
 
   return (
     <RecipeContext.Provider value={{
