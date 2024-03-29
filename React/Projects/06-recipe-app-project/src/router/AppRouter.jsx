215372618cd25f56d2b07a37d3c90be01eb00595
Burak Karaman <karaman.buraak@gmail.com>
2024-03-29 01:39:56 +0100

    Update AppRouter.jsx

--- React/Projects/06-recipe-app-project/src/router/AppRouter.jsx
+++ React/Projects/06-recipe-app-project/src/router/AppRouter.jsx
@@ -4,11 +4,13 @@ import Login from '../pages/login/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import PrivateRouter from './PrivateRouter';
+import About from '../pages/about/About'
 
 
 const AppRouter = () => {
   return (
 <BrowserRouter>
+<Navbar/>
 <Routes>
     {/* <Navbar/> */}
     <Route path="/" element={<Login/>}/>
@@ -16,7 +18,8 @@ const AppRouter = () => {
     <Route path='/home' element={<PrivateRouter/>}>
     <Route path="/home" element={<Home/>}/>
     </Route>
-    
+    <Route path="/about" element={<About />} />
+
 </Routes>
 </BrowserRouter>
 
