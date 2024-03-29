6cc4f9337da57f5c018cfe00ed1aa2d2c748161b
Burak Karaman <karaman.buraak@gmail.com>
2024-03-29 04:16:14 +0100

    Update Details.jsx

--- React/Projects/06-recipe-app-project/src/pages/details/Details.jsx
+++ React/Projects/06-recipe-app-project/src/pages/details/Details.jsx
@@ -8,15 +8,16 @@ import {
   OtherPart,
 } from "./DetailsStyles";
 import dietsvg from "../../assets/diet.svg";
+import { useLocation } from "react-router-dom";
 const Details = () => {
- 
 
+  const { state: { recipe } } = useLocation()
   return (
-    
+
     <DetailContainer>
       <HeaderContainer>
-        <h1> </h1>
-        <img  alt="" />
+        <h1>{recipe.label} </h1>
+        <img src={dietsvg} alt="" />
       </HeaderContainer>
 
       <DetailPart>
@@ -24,31 +25,55 @@ const Details = () => {
           <>Nutrients</>
 
           <p>
-      
+            {recipe.totalNutrients.CA.label}:
+            {Math.round(recipe.totalNutrients.CA.quantity)}
+            {recipe.totalNutrients.CA.unit}
+
           </p>
           <p>
-        
+            {recipe.totalNutrients.CHOCDF.label}:
+            {Math.round(recipe.totalNutrients.CHOCDF.quantity)}
+            {recipe.totalNutrients.CHOCDF.unit}
+
           </p>
           <p>
-  
+            {recipe.totalNutrients.CHOLE.label}:
+            {Math.round(recipe.totalNutrients.CHOLE.quantity)}
+            {recipe.totalNutrients.CHOLE.unit}
+
           </p>
           <p>
-          
+            {recipe.totalNutrients.ENERC_KCAL.label}:
+            {Math.round(recipe.totalNutrients.ENERC_KCAL.quantity)}
+            {recipe.totalNutrients.ENERC_KCAL.unit}
+
           </p>
-          <p></p>
-          <p>Calories: </p>
+          <p>
+        {recipe.totalWeight}
+          </p>
+          <p>Calories:{Math.round(recipe.calories)} </p>
           {/* digest key i ile bize 29 elemanlı bir dizi geliyor, biz onun 4 elemanını bastırmak için slice kullandık */}
           <p>
-           
+        {recipe.digest.slice(0,4).map((item,index)=>(
+          <p key={index}>
+            {item.label}: {Math.round(item.total)}
+          </p>
+        ))}
           </p>
         </OtherPart>
 
         <ImgContainer>
-          <img  alt="" />
+          <img src={recipe.image} alt="" />
         </ImgContainer>
-
+          {recipe.ingredientLines.map((item,index)=>(
+            <div key={index}>
+              <p>
+              {index+1} * {item}
+              </p>
+            </div>
+          ))}
         <IngredContainer>
-        
+
         </IngredContainer>
       </DetailPart>
     </DetailContainer>
