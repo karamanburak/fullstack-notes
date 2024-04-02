184d14dee01fced2465da44b79df5a4a228113a4
Burak Karaman <karaman.buraak@gmail.com>
2024-04-02 09:45:05 +0200

    Create AppRouter

--- React/Projects/07-movie-app/movie-app-inclass/src/router/AppRouter.jsx
+++ React/Projects/07-movie-app/movie-app-inclass/src/router/AppRouter.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from "../pages/Main";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Main/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/details/:id" element={<MovieDetail/>}/>
+
+
+
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default AppRouter;
