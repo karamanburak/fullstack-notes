08e3369f8f87beb5500682b246ad928ac6cca730
Burak Karaman <karaman.buraak@gmail.com>
2024-04-03 10:02:01 +0200

    Update toastNotify

--- React/Projects/07-movie-app/movie-app-inclass/src/helpers/toastNotify.js
+++ React/Projects/07-movie-app/movie-app-inclass/src/helpers/toastNotify.js
@@ -1,6 +1,6 @@
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-export const toastSuccessNotify = (msg) => {
+export const toastSuccessNotify = (msg) =>
     toast.success(msg, {
         position: "top-right",
         autoClose: 5000,
@@ -11,4 +11,24 @@ export const toastSuccessNotify = (msg) => {
         progress: undefined,
         theme: "colored",
     });
-}
+
+export const toastWarnNotify = (msg) => {
+    toast.warn(msg, {
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+};
+export const toastErrorNotify = (msg) => {
+    toast.error(msg, {
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+};
