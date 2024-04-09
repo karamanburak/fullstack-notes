82028002315061993ad2cb5dd2d4f86d24d6c67b
Burak Karaman <karaman.buraak@gmail.com>
2024-04-09 12:47:26 +0200

    Add pnpm build on local

--- React/RC-14-redux-toolkit-projects/inclass/src/app/store.js
+++ React/RC-14-redux-toolkit-projects/inclass/src/app/store.js
@@ -7,4 +7,6 @@ export const store = configureStore({
         auth:authReducer,
         products:productReducer,
     },
+    devTools: process.env.NODE_ENV !== "production"
+    //? eger gelistirme asamasi production ise o zaman yukaridaki ifade false dondurur ve dolayisiyla devTool kullanima kapali olur.
 })
