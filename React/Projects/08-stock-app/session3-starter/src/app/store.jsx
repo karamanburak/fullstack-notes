9ffcff3b946e4b74013c8dd1f777c020c1926ef6
Burak Karaman <karaman.buraak@gmail.com>
2024-04-19 08:51:52 +0200

    Add stock-app starter-3

---
+++ React/Projects/08-stock-app/session3-starter/src/app/store.jsx
@@ -0,0 +1,36 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import authReducer from "../features/authSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+  reducer: {
+    auth: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== "production",
+});
+export let persistor = persistStore(store);
+
+export default store;
