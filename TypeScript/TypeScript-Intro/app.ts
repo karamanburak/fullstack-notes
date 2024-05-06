0435663abc83a88321ae377b564b29dbbd0c40df
Burak Karaman <karaman.buraak@gmail.com>
2024-05-06 10:04:11 +0200

    Create TypeScript Intro

---
+++ TypeScript/TypeScript-Intro/app.ts
@@ -0,0 +1,9 @@
+console.log("Hello  TS");
+
+// De-08 ile typescript dersindeyiz
+
+let b: number = 3;
+let c: number = 2;
+let d = 0;
+
+const a = (b: number, c) => b * c;
---
+++ TypeScript/TypeScript-Intro/src/app.js
@@ -0,0 +1,5 @@
+console.log("Hello  TS");
+let b = 3;
+let c = 2;
+let d = 0;
+const a = (b, c) => b * c;
