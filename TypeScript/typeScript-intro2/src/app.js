23142f4ebdce21129c524c9f6f971a8393d84beb
Burak Karaman <karaman.buraak@gmail.com>
2024-05-07 11:58:10 +0200

    Update intro2

--- TypeScript/typeScript-intro2/src/app.js
+++ TypeScript/typeScript-intro2/src/app.js
@@ -1,16 +1,50 @@
 //! Interface !\\
-const person1 = {
-    name: "John",
-    age: 40,
-};
-class LMSCalendar {
-    constructor(events) {
-        this.events = events;
-    }
-    addEvents(event) {
-        this.events.push(event);
-    }
+// interface Person {
+//   name: string;
+//   readonly age: number;
+// }
+// const person1: Person = {
+//   name: "John",
+//   age: 40,
+// };
+// interface Calendar {
+//   events: string[];
+//   addEvents(event: string): void;
+// }
+// class LMSCalendar implements Calendar {
+//   constructor(public events: string[]) {}
+//   addEvents(event: string): void {
+//     this.events.push(event);
+//   }
+// }
+// const de08 = new LMSCalendar(["HTML", "CSS", "JS", "React"]);
+// const de09 = new LMSCalendar(["HTML", "CSS", "JS", "React"]);
+// const ch17 = new LMSCalendar(["HTML", "CSS", "JS", "Angular"]);
+// de08.addEvents('TS')
+// console.log(de08);
+/* interface NumKey {
+    (key: number, value: string): void
+  }
+  
+  function addKeyVal(k: number, v: string): void {
+    console.log("adding key value", k, v)
+  }
+  
+  function update(index: number, newVal: string): void {
+    console.log("updating key value", index, newVal)
+  }
+  
+  let kvp: NumKey = addKeyVal;
+  kvp(1, 'John')
+  kvp = update
+  kvp(2, 'Jane') */
+//generics
+function getArray(items) {
+    return new Array().concat(items);
 }
-const de08 = new LMSCalendar(['HTML', 'CSS', 'JS', 'REACT']);
-de08.addEvents('TS');
-console.log(de08);
+let numArr = getArray([1, 2, 3]);
+let strArr = getArray(["John", "Jane"]);
+numArr.push(4); // OK
+strArr.push("Jake"); // OK
+numArr.push("Tim"); // Compiler Error
+strArr.push(5); // Compiler Error
