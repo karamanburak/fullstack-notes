585ecf5737a3da78aa7a87719f6cfbdb375f3b80
Burak Karaman <karaman.buraak@gmail.com>
2024-05-08 10:58:18 +0200

    Update AddTodoComp component with form and save functionality, use Axios to fetch and post todo data to a mock API

--- TypeScript/ts-04-toDoApp-project/src/components/AddTodoComp.tsx
+++ TypeScript/ts-04-toDoApp-project/src/components/AddTodoComp.tsx
@@ -1,8 +1,47 @@
+import { Box, Button, TextField } from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+import { useState } from "react";
 
 const AddTodoComp = () => {
-  return (
-  <div>AddTodoComp</div>
-)
+    // const [text,setText] = useState<string>("")
+    const [text, setText] = useState("")
+    const handleClick = () => {
+        console.log(text);
+        setText("")
+
+    }
+
+    return (
+        <Box
+            sx={{
+                display: { xs: "block", sm: "flex" },
+                justifyContent: { xs: "flex-start", sm: "center" },
+                m: { xs: 1, sm: "auto" },
+                height: { xs: "120px", sm: "80px" },
+            }}
+        >
+            <TextField
+                id="outlined-basic"
+                label="New Todo"
+                color="success"
+                sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+                variant="outlined"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                inputProps={{ maxLength: 40 }}
+            />
+            <Button
+                onClick={handleClick}
+                disabled={!text.trim()}
+                variant="contained"
+                color="success"
+                sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+                endIcon={<SaveIcon />}
+            >
+                Save Todo
+            </Button>
+        </Box>
+    )
 };
 
 export default AddTodoComp;
--- TypeScript/ts-04-toDoApp-project/src/pages/Home.tsx
+++ TypeScript/ts-04-toDoApp-project/src/pages/Home.tsx
@@ -2,19 +2,62 @@
 import { Container, Typography } from '@mui/material';
 import AddTodoComp from '../components/AddTodoComp';
 import TodoList from '../components/TodoList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-interface ITodoType{
+interface ITodoType {
     task: string;
     isDone: boolean;
     id: string | number;
 }
 
+const url = "https://634ac3fc5df952851418480f.mockapi.io/api/todos"
 
 const Home = () => {
     // const [todos, setTodos] = useState([] as ITodoType[])
     // const [todos, setTodos] = useState<Array<ITodoType>>([])
     const [todos, setTodos] = useState<ITodoType[]>([]) // yaygin olan kullanim
+
+
+    const getTodos = async () => {
+        try {
+            const { data } = await axios<ITodoType[]>(url)
+            setTodos(data)
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    //! Function type tanimlama 1.yol !\\
+    // const addTodo = async (text:string) => {
+    //     try {
+            
+    //     } catch (error) {
+    //         console.log(error);
+            
+    //     }
+
+    // }
+
+
+    //! Function type tanimlama 2.yol !\\
+    type AddFn = (text:string)=> Promise<void>
+    const addTodo:AddFn = async (text) => {
+        try {
+            await axios.post(url,{task:text, isDone:false})
+            getTodos()
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+    }
+
+    useEffect(() => {
+        getTodos()
+    }, [])
 
 
     return (
