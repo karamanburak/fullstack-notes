a0102712f437befbfced94ddf3f527dd9a911792
Burak Karaman <karaman.buraak@gmail.com>
2024-05-13 20:11:49 +0200

    Add Home and Favorites pages with Navbar and update App structure

--- TypeScript/ts-05-clarus-store-project/src/App.tsx
+++ TypeScript/ts-05-clarus-store-project/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Flip, ToastContainer } from "react-toastify";
+import Home from "./pages/Home";
+import FavoritesPage from "./pages/FavoritesPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="bg-slate-500 min-h-screen text-center">
-      <p className="text-white p-20">Hello TypeScript + TailwindCss</p>
+    <div className="bg-slate-500 min-h-screen">
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/favorites" element={<FavoritesPage/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
