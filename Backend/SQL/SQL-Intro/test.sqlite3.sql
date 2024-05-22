f04ea533500f9d76cf6f652995ab8c7badd9557e
Burak Karaman <karaman.buraak@gmail.com>
2024-05-22 11:39:37 +0200

    Update test.sqlite3.sql file with additional SQL queries and comments

--- Backend/SQL/SQL-Intro/test.sqlite3.sql
+++ Backend/SQL/SQL-Intro/test.sqlite3.sql
@@ -1,35 +1,35 @@
 -- SQLite
 -- * WHERE - Filtreleme
--- select * from Album; -- tüm veriler listelenir
+-- select * from Album; --* tüm veriler listelenir
 -- select * from Album where AlbumId=1;
 -- select * from Album where Title='For Those About To Rock We Salute You';
--- select * from Customer where Country='Canada'; -- Ülkesi kanada olanlar
+-- select * from Customer where Country='Canada'; --* Ülkesi kanada olanlar
 -- SELECT * FROM Customer WHERE Country="USA";
 -- SELECT * FROM Customer WHERE CustomerId=15;
 -- SELECT * FROM Customer;
 
--- -- 1. Marktan sonraki müsterileri getir
--- SELECT * FROM Customer WHERE FirstName="Mark"; -- 1. Mark id si 14
+-- --! 1. Marktan sonraki müsterileri getir
+-- SELECT * FROM Customer WHERE FirstName="Mark"; --* 1. Mark id si 14
 -- SELECT * FROM Customer WHERE CustomerId>14;
 
--- -- 1. Mark ve sonraki müsterileri getir
+-- --! 1. Mark ve sonraki müsterileri getir
 -- SELECT * FROM Customer WHERE CustomerId>=14;
 
--- --iki Mark arasindaki müsterileri getir
--- SELECT * FROM Customer WHERE FirstName="Mark"; -- 2. Mark id si 55
+-- --! iki Mark arasindaki müsterileri getir
+-- SELECT * FROM Customer WHERE FirstName="Mark"; --* 2. Mark id si 55
 -- SELECT * FROM Customer WHERE CustomerId > 14;
 -- SELECT * FROM Customer WHERE CustomerId < 55;
 
--- --iki komutu birlestirmek icin...
+-- --! iki komutu birlestirmek icin...
 -- SELECT * FROM Customer WHERE CustomerId BETWEEN 14 AND 55;
 
--- --USA da yasamayanlari getir
+-- --! USA da yasamayanlari getir
 -- SELECT * FROM Customer WHERE Country="USA";
 -- SELECT * FROM Customer WHERE Country<>"USA";
 -- SELECT * FROM Customer WHERE Country!="USA";
 -- SELECT * FROM Customer WHERE not Country="USA";
 
--- -- Canada ve Brezilyada yasayanlari getir..
+-- --! Canada ve Brezilyada yasayanlari getir..
 -- SELECT * FROM Customer WHERE Country = "Canada";
 -- SELECT * FROM Customer WHERE Country = "Brazil";
 -- SELECT * FROM Customer WHERE Country = "Canada" or Country = "Brazil";
@@ -38,3 +38,28 @@
 -- SELECT * FROM Customer WHERE Country IN ('USA');
 -- SELECT * FROM Customer WHERE Country NOT IN ('USA');
 -- SELECT * FROM Customer WHERE CustomerId IN (1,4,8,45) AND (Country = "Brazil" OR Country = "USA") OR CustomerId = 45
+-- SELECT * FROM Customer WHERE (Country = 'USA' OR Country = 'Brazil' OR Country='Denmark') AND (CustomerId BETWEEN 25 AND 27);
+-- SELECT * FROM Customer WHERE Country IN ('USA','Brazil','Denmark') AND (CustomerId BETWEEN 25 AND 27);
+-- SELECT * FROM Customer WHERE Country = 'USA';
+-- SELECT * FROM Customer WHERE Country LIKE 'USA'; --* bu durumda eşittirden farklı bir sonuç vermez
+-- SELECT * FROM Customer WHERE Country LIKE '%U%';
+-- SELECT * FROM Customer WHERE Country LIKE 'U%'; --* U ile başlayanları getiri
+-- SELECT * FROM Customer WHERE Country LIKE '%A'; --* A ile birenler gelir
+-- SELECT * FROM Customer WHERE Country LIKE 'U_A%';
+-- SELECT * FROM Customer;
+-- SELECT * FROM Customer WHERE Country LIKE 'U%A';
+
+-- * ORDER BY
+-- SELECT * FROM Customer ORDER BY CustomerId ASC; --* ARTAN SIRALAMA
+-- SELECT * FROM Customer ORDER BY CustomerId DESC; --* AZALAN SIRALAMA
+-- SELECT * FROM Customer ORDER BY FirstName,LastName DESC WHERE CustomerId IN (14,55); --* Bu satir hata verir. WHERE Komutu ilk önce gelmelidir.
+-- SELECT * FROM Customer WHERE CustomerId IN (14,55) ORDER BY FirstName,LastName DESC;
+-- SELECT * FROM Customer WHERE CustomerId IN (14,55) ORDER BY FirstName,LastName DESC LIMIT 0,10
+-- SELECT count(*) as 'toplam kisi' FROM Customer;  --* toplam kisi
+-- SELECT SUM(Total) as total FROM Invoice;
+
+--* *** iliski durumu -- Iliskisel veri tabani ***
+-- SELECT * FROM Album  JOIN Artist  ON Album.ArtistId = Artist.ArtistId; --* orjinal hali
+-- SELECT * FROM Album a JOIN Artist r ON a.ArtistId = r.ArtistId; --* kisa yolu
+SELECT a.ArtistId,a.Title, r.Name FROM Album a JOIN Artist r ON a.ArtistId = r.ArtistId; 
+
