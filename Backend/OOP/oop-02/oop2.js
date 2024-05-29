08e5b5fc27ae6ed25d0a3aeaf4fc326d2bcc6885
Burak Karaman <karaman.buraak@gmail.com>
2024-05-29 10:02:21 +0200

    Add new example

--- Backend/OOP/oop-02/oop2.js
+++ Backend/OOP/oop-02/oop2.js
@@ -20,25 +20,98 @@
 // const PascalNamedClass = class { ... }
 
 
-const Car = class {
-    propertyName = 'value' // attribute, field
+// const Car = class {
+//     propertyName = 'value' // attribute, field
 
-    constructor(parametre1, parametre2 = 'default-value') {
-        this.parametre1 = parametre1
-        this.parametre2 = parametre2
-    } 
+//     constructor(parametre1, parametre2 = 'default-value') {
+//         this.parametre1 = parametre1
+//         this.parametre2 = parametre2
+//     } 
 
-    methodName1 () {
-        return this
-    }
+//     methodName1 () {
+//         return this
+//     }
 
-}
+// }
 
 //? INSTANCE: Class'tan üretilen nesnelere denir.
 
-const InstanceName = new Car('test')
+// const InstanceName = new Car('test')
 
-// console.log(InstanceName);
-console.log(InstanceName.methodName1());
+// console.log(InstanceName); // Asagidaki console ile ayni sonucu verir.
+// console.log(InstanceName.methodName1()); // Yukaridaki console ile ayni sonucu verir.
+
+
+//? constructor() method
+
+// class Car {
+//     isRunning = false
+//     constructor(brand, model, year) {
+//         this.brand = brand
+//         this.model = model
+//         this.year = year
+//     }
+//     runEngine() {
+//         this.isRunning = true
+//         console.log('runEngine >>','Start Engine');
+//         // return this.isRunning
+//     }
+// }
+
+// const Ford = new Car('Ford', 'Mustang', 1990) 
+
+// console.log(Ford); // Car { isRunning: false, brand: 'Ford', model: 'Mustang', year: 1990 }
+// console.log(Ford.isRunning); // false
+// Ford.runEngine()
+// console.log('Ford >>',Ford.isRunning); // true
+
+
+// const Mazda = new Car('Mazda', '323', 2000)
+
+// console.log('Mazda >>', Mazda.isRunning); // false
+// Mazda.runEngine()
+// console.log('is running >>', Mazda.isRunning );
+
+//* ------------------------------------- *//
+//? INHERITANCE: Miras alma, baska bir Class'in tüm özelliklerini devralma (paren-child iliskisi)
+//* THIS = child (current) class
+//* SUPER = parent class
+
+class Vehicle {
+    vehicleActive = false
+
+    constructor(vehicleType) {
+        this.vehicleType = vehicleType
+    }
+
+    sayHello() {
+        console.log('Hello');
+    }
+}
+
+
+class Car extends Vehicle {
+    isRunning = false
+
+    constructor(brand, model, year, vehicleType = true) {
+        super(vehicleType)
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+
+    }
+}
+
+const Mercedes = new Car('Mercedes', 'M3000', 1995)
+
+console.log('all >>', Mercedes);
+console.log(Mercedes.vehicleActive);
+Mercedes.sayHello()
+
+
 
 
