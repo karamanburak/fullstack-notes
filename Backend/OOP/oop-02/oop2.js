050dfe46c97365325a44b9a51fcc703e2b8efda4
Burak Karaman <karaman.buraak@gmail.com>
2024-05-29 10:51:50 +0200

    Refactor OOP code: add/modify classes, constructors

--- Backend/OOP/oop-02/oop2.js
+++ Backend/OOP/oop-02/oop2.js
@@ -26,7 +26,7 @@
 //     constructor(parametre1, parametre2 = 'default-value') {
 //         this.parametre1 = parametre1
 //         this.parametre2 = parametre2
-//     } 
+//     }
 
 //     methodName1 () {
 //         return this
@@ -58,7 +58,7 @@
 //     }
 // }
 
-// const Ford = new Car('Ford', 'Mustang', 1990) 
+// const Ford = new Car('Ford', 'Mustang', 1990)
 
 // console.log(Ford); // Car { isRunning: false, brand: 'Ford', model: 'Mustang', year: 1990 }
 // console.log(Ford.isRunning); // false
@@ -77,40 +77,111 @@
 //* THIS = child (current) class
 //* SUPER = parent class
 
-class Vehicle {
-    vehicleActive = false
+// class Vehicle {
+//     vehicleActive = false
 
-    constructor(vehicleType) {
-        this.vehicleType = vehicleType
-    }
+//     constructor(vehicleType) {
+//         this.vehicleType = vehicleType
+//     }
 
-    sayHello() {
-        console.log('Hello');
-    }
-}
+//     sayHello() {
+//         console.log('Hello');
+//     }
+// }
 
 
-class Car extends Vehicle {
-    isRunning = false
+// class Car extends Vehicle {
+//     isRunning = false
 
-    constructor(brand, model, year, vehicleType = true) {
-        super(vehicleType)
-        this.brand = brand
-        this.model = model
-        this.year = year
-    }
+//     constructor(brand, model, year, vehicleType = true) {
+//         super(vehicleType)
+//         this.brand = brand
+//         this.model = model
+//         this.year = year
+//     }
 
-    runEngine() {
-        this.isRunning = true
+//     runEngine() {
+//         this.isRunning = true
 
-    }
-}
+//     }
+// }
 
-const Mercedes = new Car('Mercedes', 'M3000', 1995)
+// const Mercedes = new Car('Mercedes', 'M3000', 1995)
 
-console.log('all >>', Mercedes);
-console.log(Mercedes.vehicleActive);
-Mercedes.sayHello()
+// // console.log('all >>', Mercedes);
+// // console.log(Mercedes.vehicleActive);
+// // Mercedes.sayHello()
+
+// class Accessory extends Car {
+//     constructor(accessoryName, brand, model, year) {
+//         super(brand, model, year)
+//         this.accessoryName = accessoryName
+//     }
+// }
+
+// const fordClimate = new Accessory('Bosh Climate', 'Ford', 'Mustang', 1993)
+// // console.log('all >>', fordClimate);
+
+
+/* --------------------------------------------- */
+//? Polymorphism: Miras aldığımız sınıfın özellik/methodlarını yeniden yazabilme.
+//? Override: Üst metodla aynı isim ve yapıda yeni bir metod yazma. (ezme / iptal etme / önceliğini alma)
+//? Overload: Üst metodla aynı isimde ama farklı yapıda (farklı adet/tip) yeni method oluşturma. (aynı anda ikisi de aktif) (JS desteklemez)
+
+
+// class Vehicle {
+//     vehicleActive = false 
+
+//     constructor(vehicleType) {
+//         this.vehicleActive = vehicleType
+//     }
+
+//     getDetails() {
+//         console.log('Vehicle/getDetails started ');
+//         return this
+//     }
+//     getType(vehicleType){
+//         // console.log('Vehicle Type: ' + this.vehicleType);
+//         console.log(`Vehicle type: ${this.vehicleType}`);
+        
+//     }
+// }
+
+// class Car extends Vehicle {
+//     isRunning = false
+//     constructor(brand, model, year, vehicleType = true) {
+//         super(vehicleType)
+//         this.brand = brand
+//         this.model = model
+//         this.year = year
+//     }
+
+//     runEngine() {
+//         this.isRunning = true
+//         console.log('Engine is running...');
+//         return this.isRunning
+//     }
+//     getDetails() {
+//         console.log('Car/getDetails started');
+        
+//     }
+//     getType(vehicleType,brand){
+//         console.log(`Vehicle/Brand  type ${vehicleType} ${brand}`);
+        
+//     }
+// }
+
+// const Mercedes = new Car('Mercedes', 'AMG', 2024, 'Sport')
+
+// // console.log(Mercedes); // Car vehicleActive: 'Sport', isRunning: false, brand: 'Mercedes', model: 'AMG', year: 2024
+
+// console.log(Mercedes.vehicleActive);
+
+// Mercedes.getDetails()
+
+// Mercedes.getType('Car') // Vehicle/Brand  type Car undefined
+// Mercedes.getType('Car', 'Ford') // Vehicle/Brand  type Car Ford
+
 
 
 
