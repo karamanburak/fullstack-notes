e9a81baadb6dc296964f8fcc504eacbab9485afc
Burak Karaman <karaman.buraak@gmail.com>
2024-05-31 11:31:57 +0200

    Update index.js to demonstrate different ways of importing and exporting modules

--- Backend/NodeJs/node-02-NodeJs_Modules/index.js
+++ Backend/NodeJs/node-02-NodeJs_Modules/index.js
@@ -1,4 +1,36 @@
 'use strict'
 
 console.log("module lesson on nodeJs in index.js");
-require('./modules/module')
+
+
+// require('./modules/module')
+
+
+// //! single function 
+// const testfunction = require('./modules/module')
+// testfunction()
+
+
+
+// //! multi function import 1. yol array 
+// const [test1, test3, test2] = require('./modules/module') // export sirasina göre aktarir.
+
+// test1()
+// test2()
+// test3()
+
+
+// //! multi function import 2. yol object
+// const {test1, test2, test3} = require('./modules/module')
+
+// test2()
+// test1()
+// test3()
+
+//! multi function import 3. yol object (gelen verilerin keylerini destructring ederken degistirme)
+const {test1: t1, test2:t2, test3:t3} = require('./modules/module')
+
+t2()
+t1()
+t3()
+
--- Backend/NodeJs/node-02-NodeJs_Modules/modules/module.js
+++ Backend/NodeJs/node-02-NodeJs_Modules/modules/module.js
@@ -1,3 +1,60 @@
 'use strict'
 
 console.log('module page');
+
+//! single function export 1.yol
+
+/*
+const testfunction = function ()  {
+    console.log('this is test function');
+    
+}
+
+module.exports = testfunction
+*/
+
+
+//! single function export 2.yol 
+
+/*
+module.exports=function(){
+    console.log('this is test function');
+    
+}
+*/
+ 
+
+//! multi function
+
+const testfunction1 = function () {
+    console.log('this is test function 1');
+
+}
+
+const testfunction2 = function () {
+    console.log('this is test function 2');
+
+}
+
+const testfunction3 = function () {
+    console.log('this is test function 3');
+
+}
+
+// //! multi function export 1. yol Array
+
+// module.exports = [
+//     testfunction1,
+//     testfunction2,
+//     testfunction3
+// ]
+
+
+
+//! multi function export 2. yol Object
+
+module.exports = {
+    test1: testfunction1,
+    test2: testfunction2,
+    test3: testfunction3
+}
