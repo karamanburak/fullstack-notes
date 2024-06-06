c382dfb4565f7432d4f0cbbf0c52e0692396ce57
Burak Karaman <karaman.buraak@gmail.com>
2024-06-06 11:12:21 +0200

    Replace HTTP server implementation with Express.js, add routes for '/' and '/products' endpoints

--- Backend/03-NodeJs/workshop/01-Recap1/index.js
+++ Backend/03-NodeJs/workshop/01-Recap1/index.js
@@ -1,28 +1,77 @@
+//* http seerver
 
-const http = require('http')
-const url = require('url')
+// const http = require('http')
+// const url = require('url')
 
-const routeHandler = (req, res) => {
-    console.log(req.url);
-    console.log(req.method);
-    const parsedUrl = url.parse(req.url, true)
-    console.log(parsedUrl);
-    
-    // if (req.url == '/' && req.method == "GET") {
-    //     res.write("Hello World")
-    //     res.end()
-    // }
+// const routeHandler = (req, res) => {
+//     console.log(req.url);
+//     console.log(req.method);
+//     const parsedUrl = url.parse(req.url, true)
+//     console.log(parsedUrl);
 
-    if (parsedUrl.pathname == '/' && req.method == "GET") {
-        res.write("Hello World")
-        res.end()
-    } else {
-        res.write("Not Found")
-        res.end()
-    }
+//     // if (req.url == '/' && req.method == "GET") {
+//     //     res.write("Hello World")
+//     //     res.end()
+//     // }
 
-}
+//     if (parsedUrl.pathname == '/' && req.method == "GET") {
+//         res.write("Hello World")
+//         res.end()
+//     } else {
+//         res.write("Not Found")
+//         res.end()
+//     }
 
-const server = http.createServer(routeHandler)
-server.listen(3000)
+// }
 
+// const server = http.createServer(routeHandler)
+// server.listen(3000)
+
+
+const express = require("express")
+const app = express()
+const products = require("./products.json")
+
+app.listen(3000, function () {
+    console.log("server started");
+
+})
+
+
+//* app.use() methodunda okuma siralamasi önemlidir!!!
+// app.use("/products", (req, res) => {
+//     if (req.method == "GET") {
+//         res.send("Hello GET Products")
+//     } else if (req.method == "POST") {
+//         res.send("Hello POST")
+//     } else {
+//         res.send("Not Found")
+//     }
+// })
+
+// app.use("/", (req,res)=>{
+//     if(req.method == "GET"){
+//         res.send("Hello GET")
+//         }else if (req.method == "POST") {
+//         res.send("Hello POST")
+//     } else {
+//         res.send("Not Found")
+//     }
+// })
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "Hello World"
+    })
+})
+app.get("/products", (req, res) => {
+    console.log(req.query);
+    // const page = req.query.page || 1
+    // const limit = req.query.limit || 10
+    const { page = 1, limit = 10, category = "" } = req.query
+
+    res.send({
+        message: "Hello Products",
+        products: products.filter(item=>item.category == category).slice((page - 1) * limit, page * limit)
+    })
+})
