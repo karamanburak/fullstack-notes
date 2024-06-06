1a4b22998bac88e6f725f0b46c6a261fb67d4c87
Burak Karaman <karaman.buraak@gmail.com>
2024-06-06 10:13:17 +0200

    Add workschop recap1; Create server with nodeJS

---
+++ Backend/03-NodeJs/workshop/01-Recap1/index.js
@@ -0,0 +1,28 @@
+
+const http = require('http')
+const url = require('url')
+
+const routeHandler = (req, res) => {
+    console.log(req.url);
+    console.log(req.method);
+    const parsedUrl = url.parse(req.url, true)
+    console.log(parsedUrl);
+    
+    // if (req.url == '/' && req.method == "GET") {
+    //     res.write("Hello World")
+    //     res.end()
+    // }
+
+    if (parsedUrl.pathname == '/' && req.method == "GET") {
+        res.write("Hello World")
+        res.end()
+    } else {
+        res.write("Not Found")
+        res.end()
+    }
+
+}
+
+const server = http.createServer(routeHandler)
+server.listen(3000)
+
