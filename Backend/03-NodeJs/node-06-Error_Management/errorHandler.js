685b41cab938b7f1ba29084b1425620c233159f4
Burak Karaman <karaman.buraak@gmail.com>
2024-06-07 11:17:21 +0200

    Update touch.js file: add comments, remove and rearrange code blocks, and add new error handling logic"

---
+++ Backend/03-NodeJs/node-06-Error_Management/errorHandler.js
@@ -0,0 +1,18 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - ERROR MANAGEMENT
+------------------------------------------------------- */
+//* ERROR HANDLER
+
+//? errorHandler is middleware and has must be four parameters. (error, request, response, next)
+
+module.exports = (err, req, res, next) => {
+    const errorStatusCode = res?.errorStatusCode || 500
+    res.status(errorStatusCode).send({
+        error: true,
+        status: false,
+        message: err.message,
+        cause: err.cause,
+        stack: err.stack
+    })
+}
--- Backend/03-NodeJs/node-06-Error_Management/touch.js
+++ Backend/03-NodeJs/node-06-Error_Management/touch.js
@@ -1,4 +1,7 @@
 'use strict'
+/* -------------------------------------------------------
+            EXPRESSJS - ERROR MANAGEMENT
+------------------------------------------------------- */
 
 const express = require('express')
 const app = express()
@@ -14,23 +17,63 @@ const HOST = process.env.HOST || '127.0.0.1'
 //* ---------------------------------------------------------------- *//
 
 //* ---------------------------------------------------------------- *//
-app.get('/', (req, res) => {
-    throw new Error('Error New Message')
+// app.get('/', (req, res) => {
+//     throw new Error('Error New Message')
+// })
+
+// app.get('/user/:id', (req, res, next) => {
+//     const id = req.params?.id || 0
+
+//     try {
+//         if (isNaN(id)) {
+//             throw new Error('Id is not a number')
+//         }
+//         res.send({ status: true, error: false })
+//     } catch (error) {
+//         res.send({ status: false, error: true })
+//     }
+
+// })
+//* ---------------------------------------------------------------- *//
+
+//* ---------------------------------------------------------------- *//
+//^ ----------------------//^ ERROR HANDLER------------------------- ^//
+//? Control with catch(next)
+
+// app.get('/', (req, res) => {
+//     throw new Error('error')
+//     res.send('true')
+// })
+
+//* ---------------------------------------------------------------- *//
+
+//* ---------------------------------------------------------------- *//
+
+const asyncFunction = async () => {
+    throw new Error('Error is async-function', { cause: 'No reason :)' })
+}
+app.get('/async', async (req, res, next) => {
+    res.errorStatusCode = 501
+
+    //await asyncFunction().then().catch((err) => { next(err) })
+    await asyncFunction().then().catch(next)
 })
 
-app.get('/user/:id', (req, res, next) => {
-    const id = req.params?.id || 0
+// const errorHandler = (err, req, res, next) => {
+//     const errorStatusCode = res?.errorStatusCode || 500
+//     res.status(errorStatusCode).send({
+//         error: true,
+//         status: false,
+//         message: err.message,
+//         cause: err.cause,
+//         stack: err.stack
+//     })
+// }
 
-    try {
-        if (isNaN(id)) {
-            throw new Error('Id is not a number')
-        }
-        res.send({ status: true, error: false })
-    } catch (error) {
-        res.send({ status: false, error: true })
-    }
+// app.use(errorHandler)
 
-})
+//* ---------------------------------------------------------------- *//
+app.use(require('./errorHandler'))
 //* ---------------------------------------------------------------- *//
 
 app.listen(PORT, () => {
