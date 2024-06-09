03d124b869abf3d5d7b91f7dc1e40b3df3f8dd18
Burak Karaman <karaman.buraak@gmail.com>
2024-06-09 09:37:23 +0200

    Add newFiles

--- Backend/03-NodeJs/Exercises/01-reading-writing-files-sync-async/index.js
+++ Backend/03-NodeJs/Exercises/01-reading-writing-files-sync-async/index.js
@@ -8,4 +8,5 @@ const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on 
 fs.writeFileSync('./txt/output.txt', textOut)
 
 // Non-Blocking, asycnchronous way
+fs.readFile('./txt/start.txt')
 
