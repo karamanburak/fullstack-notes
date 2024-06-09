8683a17be51e02458b5607dbc374b5963553cff3
Burak Karaman <karaman.buraak@gmail.com>
2024-06-09 09:35:36 +0200

    Blocking, sycnchronous way

---
+++ Backend/03-NodeJs/Exercises/01-reading-writing-files-sync-async/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+// Blocking, sycnchronous way
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+fs.writeFileSync('./txt/output.txt', textOut)
+
+// Non-Blocking, asycnchronous way
+
