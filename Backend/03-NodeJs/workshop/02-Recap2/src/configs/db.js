868866a94af92e8019849a1f0e602cff1b8a52e5
Burak Karaman <karaman.buraak@gmail.com>
2024-06-12 09:46:17 +0200

    Add Sequelize configuration and Contact model definition

--- Backend/03-NodeJs/workshop/02-Recap2/src/configs/db.js
+++ Backend/03-NodeJs/workshop/02-Recap2/src/configs/db.js
@@ -1,1 +1,13 @@
 //* db connect
+
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("sqlite:./db.sqlite3");
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+module.exports = { sequelize };
--- Backend/03-NodeJs/workshop/02-Recap2/src/models/contactModel.js
+++ Backend/03-NodeJs/workshop/02-Recap2/src/models/contactModel.js
@@ -1,1 +1,34 @@
 //* Contact Model
+
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../configs/db");
+
+const Contact = sequelize.define("contacts", {
+  fistName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  address: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+});
