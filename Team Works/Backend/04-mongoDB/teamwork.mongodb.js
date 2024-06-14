20dd74911da76627fe4ef8ddea895b3303ae5365
Burak Karaman <karaman.buraak@gmail.com>
2024-06-14 15:48:43 +0200

    Add new mongodb collection

--- Team Works/Backend/04-mongoDB/teamwork.mongodb.js
+++ Team Works/Backend/04-mongoDB/teamwork.mongodb.js
@@ -1,3 +1,26 @@
+db.userlist.insertMany([
+  { user_id: "user_abc", status: "A", age: 25 },
+  { user_id: "user_def", status: "B", age: 30 },
+  { user_id: "user_ghi", status: "C", age: 22 },
+  { user_id: "user_jkl", status: "D", age: 28 },
+  { user_id: "user_mno", status: "A", age: 55 },
+  { user_id: "user_pqr", status: "B", age: 40 },
+  { user_id: "user_stu", status: "C", age: 26 },
+  { user_id: "user_vwx", status: "D", age: 32 },
+  { user_id: "user_yza", status: "A", age: 24 },
+  { user_id: "user_bcd", status: "B", age: 29 },
+  { user_id: "user_efg", status: "C", age: 58 },
+  { user_id: "user_hij", status: "D", age: 33 },
+  { user_id: "user_klm", status: "A", age: 31 },
+  { user_id: "user_nop", status: "B", age: 23 },
+  { user_id: "user_qrs", status: "C", age: 34 },
+  { user_id: "user_tuv", status: "D", age: 21 },
+  { user_id: "user_wxy", status: "A", age: 38 },
+  { user_id: "user_zab", status: "B", age: 63 },
+  { user_id: "user_cde", status: "C", age: 39 },
+  { user_id: "user_fgh", status: "D", age: 20 },
+]);
+
 db.people.insertMany([
   { id: 1, user_id: "user_abc", status: "A", age: 25 },
   { id: 2, user_id: "user_def", status: "B", age: 30 },
