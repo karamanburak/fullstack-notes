fc3b1fdf42292da27d6335fa31e078dc28cf90d3
Burak Karaman <karaman.buraak@gmail.com>
2024-06-28 10:02:18 +0200

    Add tutorial controller and connected to MongoDB using Mongoose

---
+++ Backend/03-NodeJs/Projects/03-Deployment-Tutorialapp/backend/models/tutorialModel.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const turorialSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    collection: "tutorial",
+    timestamps: true,
+  }
+);
+
+tutorialSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+// tutorialSchema.set("toJSON", {
+//   transform: function (doc, ret, options) {
+//     ret.id = ret._id;
+//     delete ret._id;
+//     delete ret.__v;
+//   },
+// });
+
+const Tutorial = mongoose.model("Tutorial", tutorialSchema);
+
+module.exports = Tutorial;
