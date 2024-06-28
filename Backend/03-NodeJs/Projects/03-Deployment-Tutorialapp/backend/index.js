a25a0cebcc71888d534d1aa31eb1ca57acd5f277
Burak Karaman <karaman.buraak@gmail.com>
2024-06-28 10:48:53 +0200

    Add CORS middleware to backend, updated package.json and package-lock.json, and modify frontend API URLs to point to localhost:8000

--- Backend/03-NodeJs/Projects/03-Deployment-Tutorialapp/backend/index.js
+++ Backend/03-NodeJs/Projects/03-Deployment-Tutorialapp/backend/index.js
@@ -1,12 +1,24 @@
 "use strict";
 require("express-async-errors");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const { mongooseConnection } = require("./startup/mongooseConnection");
 const PORT = process.env?.PORT || 8000;
 
 const app = express();
 app.use(express.json());
+
+//! cors
+// app.use(cors()); //* tüm istemci urllerine ve tüm http methodlara izin verir.
+
+//* specific
+app.use(
+  cors({
+    origin: "http://localhost:3000", //* sondaki slash olmayacak. sadece localhost:3000 e izin ver. Host adresi
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", //* methodlara verilecek izinleri ayarlanabilir.
+  })
+);
 
 app.all("/", (req, res) => {
   res.send("Hello TutorialApp");
