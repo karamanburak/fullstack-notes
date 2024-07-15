d92348979a8be2efae58c06cf4a1cbdf0aa3b19a
Burak Karaman <karaman.buraak@gmail.com>
2024-07-15 12:03:05 +0200

    Add three permission middleware functions: isLogin, isStafforAdmin, and isAdmin

--- Backend/03-NodeJs/Projects/06-FlightApi/src/middlewares/permissions.js
+++ Backend/03-NodeJs/Projects/06-FlightApi/src/middlewares/permissions.js
@@ -1,10 +1,41 @@
-"use strict"
+"use strict";
+
+const { CustomError } = require("../errors/customError");
 
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 
 module.exports = {
-
-    
-}
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      throw new CustomError("NoPermission: You must login!", 403);
+    }
+  },
+  isStafforAdmin: (req, res, next) => {
+    if (
+      req.user &&
+      req.user.isActive &&
+      (req.user.isStaff || req.user.isAdmin)
+    ) {
+      next();
+    } else {
+      throw new CustomError(
+        "NoPermission: You must login and to be Staff or Admin!",
+        403
+      );
+    }
+  },
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      throw new CustomError(
+        "NoPermission: You must login and to be Staff or Admin!",
+        403
+      );
+    }
+  },
+};
