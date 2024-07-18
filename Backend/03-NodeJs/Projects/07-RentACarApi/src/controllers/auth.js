61ff689b289aa06c967a90694e621cf6a3173701
Burak Karaman <karaman.buraak@gmail.com>
2024-07-18 11:54:57 +0200

    Formatting changes in RentACarApi

--- Backend/03-NodeJs/Projects/07-RentACarApi/src/controllers/auth.js
+++ Backend/03-NodeJs/Projects/07-RentACarApi/src/controllers/auth.js
@@ -1,18 +1,17 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Auth Controller
 
-const User = require('../models/user')
-const Token = require('../models/token')
-const passwordEncrypt = require('../helpers/passwordEncrypt')
-const jwt = require('jsonwebtoken')
+const User = require("../models/user");
+const Token = require("../models/token");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-
-    login: async (req, res) => {
-        /*
+  login: async (req, res) => {
+    /*
             #swagger.tags = ["Authentication"]
             #swagger.summary = "Login"
             #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
@@ -26,73 +25,70 @@ module.exports = {
             }
         */
 
-        const { username, password } = req.body
+    const { username, password } = req.body;
 
-        if (username && password) {
+    if ((username || email) && password) {
+      const user = await User.findOne({ $or: [{ email }, { username }] });
 
-            const user = await User.findOne({ username })
+      if (user && user.password == passwordEncrypt(password)) {
+        if (user.isActive) {
+          /* TOKEN */
 
-            if (user && user.password == passwordEncrypt(password)) {
+          let tokenData = await Token.findOne({ userId: user._id });
 
-                if (user.isActive) {
+          if (!tokenData) {
+            const tokenKey = passwordEncrypt(user._id + Date.now());
+            tokenData = await Token.create({
+              userId: user._id,
+              token: tokenKey,
+            });
+          }
 
-                    /* TOKEN */
+          /* TOKEN */
 
-                    let tokenData = await Token.findOne({ userId: user._id })
+          /* JWT */
 
-                    if (!tokenData) {
-                        const tokenKey = passwordEncrypt(user._id + Date.now())
-                        tokenData = await Token.create({ userId: user._id, token: tokenKey })
-                    }
+          const accessData = user.toJSON(); // Valuable data.
+          const accessTime = "1m";
+          const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, {
+            expiresIn: accessTime,
+          });
+          // console.log('accessToken', accessToken)
 
-                    /* TOKEN */
+          const refreshData = { id: user._id, password: user.password }; // Checkable data.
+          const refreshTime = "3d";
+          const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, {
+            expiresIn: refreshTime,
+          });
+          // console.log('refreshToken', refreshToken)
 
-                    /* JWT */
+          /* JWT */
 
-                    const accessData = user.toJSON() // Valuable data.
-                    const accessTime = '1m'
-                    const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: accessTime })
-                    // console.log('accessToken', accessToken)
-
-                    const refreshData = { id: user._id, password: user.password } // Checkable data.
-                    const refreshTime = '3d'
-                    const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, { expiresIn: refreshTime })
-                    // console.log('refreshToken', refreshToken)
-
-                    /* JWT */
-
-                    res.status(200).send({
-                        error: false,
-                        token: tokenData.token,
-                        bearer: {
-                            access: accessToken,
-                            refresh: refreshToken
-                        },
-                        user
-                    })
-
-                } else {
-
-                    res.errorStatusCode = 401
-                    throw new Error('This account is not active.')
-                }
-
-            } else {
-
-                res.errorStatusCode = 401
-                throw new Error('Wrong username or password.')
-            }
-
+          res.status(200).send({
+            error: false,
+            token: tokenData.token,
+            bearer: {
+              access: accessToken,
+              refresh: refreshToken,
+            },
+            user,
+          });
         } else {
-
-            res.errorStatusCode = 401
-            throw new Error('Please enter username and password.')
+          res.errorStatusCode = 401;
+          throw new Error("This account is not active.");
         }
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("Wrong username or password.");
+      }
+    } else {
+      res.errorStatusCode = 401;
+      throw new Error("Please enter username and password.");
+    }
+  },
 
-    },
-
-    refresh: async (req, res) => {
-        /*
+  refresh: async (req, res) => {
+    /*
             #swagger.tags = ['Authentication']
             #swagger.summary = 'JWT: Refresh'
             #swagger.description = 'Refresh accessToken with refreshToken'
@@ -107,80 +103,70 @@ module.exports = {
             }
         */
 
-        const refreshToken = req.body?.bearer?.refresh
+    const refreshToken = req.body?.bearer?.refresh;
 
-        if (refreshToken) {
+    if (refreshToken) {
+      const jwtData = await jwt.verify(refreshToken, process.env.REFRESH_KEY);
 
-            const jwtData = await jwt.verify(refreshToken, process.env.REFRESH_KEY)
+      if (jwtData) {
+        const { id, password } = jwtData;
 
-            if (jwtData) {
+        if (id && password) {
+          const user = await User.findOne({ _id: id });
 
-                const { id, password } = jwtData
+          if (user && user.password == password) {
+            if (user.isActive) {
+              // JWT AccessToken:
+              const accessToken = jwt.sign(
+                user.toJSON(),
+                process.env.ACCESS_KEY,
+                { expiresIn: "30m" }
+              );
 
-                if (id && password) {
-
-                    const user = await User.findOne({ _id: id })
-
-                    if (user && user.password == password) {
-
-                        if (user.isActive) {
-
-                            // JWT AccessToken:
-                            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_KEY, { expiresIn: '30m' })
-
-                            res.status(200).send({
-                                error: false,
-                                bearer: {
-                                    access: accessToken
-                                }
-                            })
-
-                        } else {
-
-                            res.errorStatusCode = 401
-                            throw new Error('This account is not active.')
-                        }
-                    } else {
-
-                        res.errorStatusCode = 401
-                        throw new Error('Wrong id or password.')
-                    }
-                } else {
-
-                    res.errorStatusCode = 401
-                    throw new Error('There is not id and password in refreshToken.')
-                }
+              res.status(200).send({
+                error: false,
+                bearer: {
+                  access: accessToken,
+                },
+              });
             } else {
-
-                res.errorStatusCode = 401
-                throw new Error('sa')
+              res.errorStatusCode = 401;
+              throw new Error("This account is not active.");
             }
-
+          } else {
+            res.errorStatusCode = 401;
+            throw new Error("Wrong id or password.");
+          }
         } else {
-            
-            res.errorStatusCode = 401
-            throw new Error('Please enter token.refresh')
+          res.errorStatusCode = 401;
+          throw new Error("There is not id and password in refreshToken.");
         }
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("sa");
+      }
+    } else {
+      res.errorStatusCode = 401;
+      throw new Error("Please enter token.refresh");
+    }
+  },
 
-    },
-
-    logout: async (req, res) => {
-        /*
+  logout: async (req, res) => {
+    /*
             #swagger.tags = ["Authentication"]
             #swagger.summary = "SimpleToken: Logout"
             #swagger.description = 'Delete token key.'
         */
 
-        const auth = req.headers?.authorization || null // Token ...tokenKey...
-        const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...']
+    const auth = req.headers?.authorization || null; // Token ...tokenKey...
+    const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
 
-        const tokenData = await Token.deleteOne({ token: tokenKey[1] })
+    const tokenData = await Token.deleteOne({ token: tokenKey[1] });
 
-        res.send({
-            error: false,
-            message: 'Logout was OK.',
-            data: tokenData
-        })
-    },
-
-}
+    res.send({
+      error: false,
+      message: "Logout was OK.",
+      data: tokenData,
+    });
+  },
+};
--- Backend/03-NodeJs/Projects/07-RentACarApi/src/routes/reservation.js
+++ Backend/03-NodeJs/Projects/07-RentACarApi/src/routes/reservation.js
@@ -1,7 +1,28 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 // routes/reservation:
+
+const reservation = require("../controllers/reservation");
+
+const permissions = require("../middlewares/permissions");
+
+// URL: /reservations
+
+router
+  .route("/")
+  .get(permissions.isLogin, reservation.list)
+  .post(permissions.isLogin, reservation.create);
+
+router
+  .route("/:id")
+  .get(permissions.isLogin, reservation.read)
+  .put(permissions.isStaff, reservation.update)
+  .patch(permissions.isStaff, reservation.update)
+  .delete(permissions.isAdmin, reservation.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
