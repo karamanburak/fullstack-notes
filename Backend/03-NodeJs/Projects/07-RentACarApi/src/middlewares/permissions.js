68af21e55893cdc3be95bbffd32fb79e33dbe764
Burak Karaman <karaman.buraak@gmail.com>
2024-07-18 11:11:36 +0200

    Update car controller, permissions middleware, car and user routes, and swagger autogen configuration

--- Backend/03-NodeJs/Projects/07-RentACarApi/src/middlewares/permissions.js
+++ Backend/03-NodeJs/Projects/07-RentACarApi/src/middlewares/permissions.js
@@ -1,28 +1,40 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Middleware: permissions
 
 module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
+    }
+  },
 
-    isLogin: (req, res, next) => {
+  isStaff: (req, res, next) => {
+    // return next()
 
-        if (req.user) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login.')
-        }
-    },
+    if (
+      req.user &&
+      req.user.isActive &&
+      (req.user.isAdmin || req.user.isStaff)
+    ) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
 
-    isAdmin: (req, res, next) => {
-
-        if (req.user && req.user.isAdmin) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin.')
-        }
-    },
-}
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+};
--- Backend/03-NodeJs/Projects/07-RentACarApi/src/routes/car.js
+++ Backend/03-NodeJs/Projects/07-RentACarApi/src/routes/car.js
@@ -10,4 +10,13 @@ const car = require("../controllers/car");
 
 const permissions = require("../middlewares/permissions");
 
+// URL: /cars
+router.route("/").get(car.list).post(permissions.isStaff, car.create);
+router
+  .route("/:id")
+  .get(car.read)
+  .put(permissions.isStaff, car.update)
+  .patch(permissions.isStaff, car.update)
+  .delete(permissions.isAdmin, car.delete);
+
 module.exports = router;
